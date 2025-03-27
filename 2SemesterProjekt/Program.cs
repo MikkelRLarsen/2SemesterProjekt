20ad02f905e4982e8951ede9815a6890012bb74d
MikkelRLarsen <Thenuker95@gmail.com>
2025-03-27 12:15:41 +0100

    Add project files.

---
+++ 2SemesterProjekt/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace _2SemesterProjektWinForms
+{
+    internal static class Program
+    {
+		private static ServiceProvider _serviceProvider;
+
+		/// <summary>
+		///  The main entry point for the application.
+		/// </summary>
+		[STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            Initialize();
+            Application.Run(new Form1());
+        }
+
+		private static void Initialize()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.SetHighDpiMode(HighDpiMode.SystemAware);
+
+			var services = new ServiceCollection();
+
+			// Register services
+			// services.AddSingleton<Interface1, Class1>();
+
+
+			_serviceProvider = services.BuildServiceProvider();
+		}
+	}
+}
