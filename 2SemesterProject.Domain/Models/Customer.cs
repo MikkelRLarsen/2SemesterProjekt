b69989fa571be2bab8f5955039c6a67589791ab6
MikkelRLarsen <Thenuker95@gmail.com>
2025-03-27 12:46:44 +0100

    Customer Properties, Constructor and simple CustomerInformationValid Method

--- 2SemesterProject.Domain/Models/Customer.cs
+++ 2SemesterProject.Domain/Models/Customer.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace _2SemesterProject.Domain.Models
 {
 	internal class Customer
 	{
+		public int CustomerID { get; private set; }
+		public string CustomerName { get; private set; }
+
+		public Customer(int customerID, string customerName)
+		{
+			CustomerID = customerID;
+			CustomerName = customerName;
+
+			if (!CustomerInformationValid()) throw new ArgumentException("Customer Information not valid");
+		}
+
+		/// <summary>
+		/// Checks if CustomerID is greater than 0 and if CustomerName contains any numbers or speciel characters
+		/// </summary>
+		/// <returns>Bool</returns>
+		protected bool CustomerInformationValid()
+		{
+			Debug.Assert(CustomerID != null, "CustomerID was null");
+			Debug.Assert(CustomerName != null, "CustomerName was null");
+
+			if (CustomerID == 0) return false;
+			if (CustomerName.Any(ch => !char.IsLetterOrDigit(ch))) return false;
+			if (CustomerName.Any(ch => !char.IsDigit(ch))) return false;
+
+			return true;
+
+		}
 	}
 }
