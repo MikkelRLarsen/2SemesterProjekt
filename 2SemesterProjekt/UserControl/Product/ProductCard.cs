be1c3527252b166d69380d04893d1de6ca4e3456
MikkelSchmidtNielsen <mikkelschmidt@msn.com>
2025-04-10 15:00:12 +0200

    Made ProductCard

---
+++ 2SemesterProjekt/UserControl/Product/ProductCard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _2SemesterProject.Domain.Models;
+
+namespace _2SemesterProjekt
+{
+    public partial class ProductCard: UserControl
+    {
+        private Product _product;
+        
+        public ProductCard(Product product)
+        {
+            InitializeComponent();
+            _product = product;
+            InitializeUIDesign();
+        }
+
+        private void InitializeUIDesign()
+		{
+            productIdLabel.Text = _product.ProductID.ToString();
+            productNameLabel.Text = _product.ProductName;
+            productPriceLabel.Text = _product.ProductPrice.ToString();
+        }
+    }
+}
