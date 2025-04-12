c35c65e8df7df778dee017ca1e862511dcd010f8
MikkelRLarsen <Thenuker95@gmail.com>
2025-04-12 21:03:40 +0200

    Updated EF Repo

--- 2SemesterProjekt.Repository/EntityFrameworkRepository/EntityFrameworkRepositoryCore.cs
+++ 2SemesterProjekt.Repository/EntityFrameworkRepository/EntityFrameworkRepositoryCore.cs
@@ -20,8 +20,56 @@ namespace _2SemesterProjekt.Repository.EntityFrameworkRepository
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			base.OnConfiguring(optionsBuilder);
+			optionsBuilder.UseSqlServer(GetConnectionString());
+		}
 
+		/// <summary>
+		/// Gets Connectionstring from local folder on Desktop
+		/// </summary>
+		/// <returns></returns>
+		protected static string GetConnectionString()
+		{
+			string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+			string fullName = Path.Combine(desktopPath, "RecipeAppConnectionString");
+			return File.ReadAllText(fullName);
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Customer>().ToTable("Customer");
+			modelBuilder.Entity<Employee>().ToTable("Employee");
+			modelBuilder.Entity<Pet>().ToTable("Pet");
+			modelBuilder.Entity<Examination>().ToTable("Examination");
+			modelBuilder.Entity<ExaminationType>().ToTable("ExaminationType");
+
+			// Relations
+			modelBuilder.Entity<Pet>()
+				.HasOne(pet => pet.Employee)
+				.WithMany(cu => cu.Pets)
+				.HasForeignKey(pet => pet.CustomerID);
+
+			modelBuilder.Entity<Pet>()
+				.HasOne(pet => pet.Employee)
+				.WithMany(em => em.Pets)
+				.HasForeignKey(pet => pet.EmployeeID);
+
+			modelBuilder.Entity<Pet>()
+				.HasMany(pet => pet.Examinations)
+				.WithOne(ex => ex.Pet)
+				.HasForeignKey(pet => pet.ExaminationID);
+
+			modelBuilder.Entity<Examination>()
+				.HasOne(ex => ex.ExaminationType)
+				.WithMany(ext => ext.Examination)
+				.HasForeignKey(ex => ex.ExaminationTypeID);
+
+
+			// Primary Keys
+			modelBuilder.Entity<Customer>().HasKey(c => c.CustomerID);
+			modelBuilder.Entity<Employee>().HasKey(em => em.EmployeeID);
+			modelBuilder.Entity<Pet>().HasKey(p => p.PetID);
+			modelBuilder.Entity<Examination>().HasKey(ex => ex.ExaminationID);
+			modelBuilder.Entity<ExaminationType>().HasKey(ext => ext.ExaminationTypeID);
 		}
 	}
 }
