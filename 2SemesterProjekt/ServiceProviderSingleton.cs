df5cd72427b17f7733405b6477317586e50559f0
MikkelRLarsen <Thenuker95@gmail.com>
2025-04-25 09:06:27 +0200

    Added IoC Container as a Singleton

---
+++ 2SemesterProjekt/ServiceProviderSingleton.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2SemesterProjekt
+{
+	internal class ServiceProviderSingleton
+	{
+		private static ServiceProvider? _serviceProvider;
+
+		private ServiceProviderSingleton()
+		{
+			var services = new ServiceCollection();
+
+			// Register services
+			// Bll Services
+			//services.AddScoped<ICustomerService, CustomerService>(); 
+
+
+			// Dal Services
+			// services.AddTransient<ICustomerRepository, CustomerRepository>();
+
+
+			_serviceProvider = services.BuildServiceProvider();
+		}
+
+
+		/// <summary>
+		/// A singleton class which returns all services if they have Initalized otherwise create ServiceProdiver 
+		/// and save it as a static field for later use to preserve state
+		/// </summary>
+		/// <returns>ServiceProdivder which contains all Services with Lifetime</returns>
+		public static ServiceProvider GetServiceProvider()
+		{
+			if (_serviceProvider == null)
+			{
+				new ServiceProviderSingleton();
+			}
+
+			Debug.Assert(_serviceProvider != null, "_serviceProvider in ServiceProviderSingletong was null, somehow");
+			return _serviceProvider;
+		}
+	}
+}
