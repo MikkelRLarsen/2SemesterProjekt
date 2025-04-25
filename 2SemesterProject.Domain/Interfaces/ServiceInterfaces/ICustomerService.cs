4924423949e6dbfa9d309da2fba2081065be8b5a
MikkelRLarsen <Thenuker95@gmail.com>
2025-04-25 09:04:03 +0200

    Added all Interfaces

--- 2SemesterProject.Domain/Interfaces/ServiceInterfaces/ICustomerService.cs
+++ 2SemesterProject.Domain/Interfaces/ServiceInterfaces/ICustomerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _2SemesterProject.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace _2SemesterProject.Domain.Interfaces.ServiceInterfaces
 {
 	public interface ICustomerService
 	{
+		public Task<IEnumerable<Customer>> GetAllCustomersAsync();
+		public Customer GetCustomerById(int customerID);
+		public void UpdateCustomer(Customer customer);
+		public void DeleteCustomer(Customer customer);
+		public void CreateCustomer(Customer customer);
 	}
 }
---
+++ 2SemesterProject.Domain/Interfaces/ServiceInterfaces/IExaminationService.cs
@@ -0,0 +1,18 @@
+﻿using _2SemesterProject.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2SemesterProject.Domain.Interfaces.ServiceInterfaces
+{
+	public interface IExaminationService
+	{
+		public Task<IEnumerable<Examination>> GetAllExaminationsAsync();
+		public Examination GetExaminationById(int examinationId);
+		public void UpdateExamination(Examination examination);
+		public void DeleteExamination(Examination examination);
+		public void CreateExamination(Examination examination);
+	}
+}
