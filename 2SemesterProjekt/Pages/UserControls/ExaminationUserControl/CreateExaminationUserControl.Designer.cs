6c82f283b1188a32babb3d604287f24abcc20393
MikkelRLarsen <Thenuker95@gmail.com>
2025-04-25 11:11:43 +0200

    Added textboxes foreach value

---
+++ 2SemesterProjekt/Pages/UserControls/ExaminationUserControl/CreateExaminationUserControl.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace _2SemesterProjekt.Pages.UserControls.ExaminationUserControl
+{
+	partial class CreateExaminationUserControl
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			label5 = new Label();
+			label1 = new Label();
+			comboBox1 = new ComboBox();
+			comboBox2 = new ComboBox();
+			label2 = new Label();
+			comboBox3 = new ComboBox();
+			label3 = new Label();
+			comboBox4 = new ComboBox();
+			label4 = new Label();
+			comboBox5 = new ComboBox();
+			label6 = new Label();
+			comboBox6 = new ComboBox();
+			label7 = new Label();
+			SuspendLayout();
+			// 
+			// label5
+			// 
+			label5.AutoSize = true;
+			label5.Font = new Font("Gadugi", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			label5.Location = new Point(204, 149);
+			label5.Name = "label5";
+			label5.Size = new Size(50, 19);
+			label5.TabIndex = 4;
+			label5.Text = "Navn:";
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Font = new Font("Gadugi", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			label1.Location = new Point(53, 31);
+			label1.Name = "label1";
+			label1.Size = new Size(610, 77);
+			label1.TabIndex = 5;
+			label1.Text = "Opret Konsultation";
+			// 
+			// comboBox1
+			// 
+			comboBox1.FormattingEnabled = true;
+			comboBox1.Location = new Point(260, 149);
+			comboBox1.Name = "comboBox1";
+			comboBox1.Size = new Size(207, 23);
+			comboBox1.TabIndex = 6;
+			// 
+			// comboBox2
+			// 
+			comboBox2.FormattingEnabled = true;
+			comboBox2.Location = new Point(260, 189);
+			comboBox2.Name = "comboBox2";
+			comboBox2.Size = new Size(207, 23);
+			comboBox2.TabIndex = 8;
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Font = new Font("Gadugi", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			label2.Location = new Point(204, 189);
+			label2.Name = "label2";
+			label2.Size = new Size(50, 19);
+			label2.TabIndex = 7;
+			label2.Text = "Navn:";
+			// 
+			// comboBox3
+			// 
+			comboBox3.FormattingEnabled = true;
+			comboBox3.Location = new Point(260, 230);
+			comboBox3.Name = "comboBox3";
+			comboBox3.Size = new Size(207, 23);
+			comboBox3.TabIndex = 10;
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Font = new Font("Gadugi", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			label3.Location = new Point(204, 230);
+			label3.Name = "label3";
+			label3.Size = new Size(50, 19);
+			label3.TabIndex = 9;
+			label3.Text = "Navn:";
+			// 
+			// comboBox4
+			// 
+			comboBox4.FormattingEnabled = true;
+			comboBox4.Location = new Point(260, 270);
+			comboBox4.Name = "comboBox4";
+			comboBox4.Size = new Size(207, 23);
+			comboBox4.TabIndex = 12;
+			// 
+			// label4
+			// 
+			label4.AutoSize = true;
+			label4.Font = new Font("Gadugi", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			label4.Location = new Point(204, 270);
+			label4.Name = "label4";
+			label4.Size = new Size(50, 19);
+			label4.TabIndex = 11;
+			label4.Text = "Navn:";
+			// 
+			// comboBox5
+			// 
+			comboBox5.FormattingEnabled = true;
+			comboBox5.Location = new Point(260, 316);
+			comboBox5.Name = "comboBox5";
+			comboBox5.Size = new Size(207, 23);
+			comboBox5.TabIndex = 14;
+			// 
+			// label6
+			// 
+			label6.AutoSize = true;
+			label6.Font = new Font("Gadugi", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			label6.Location = new Point(204, 316);
+			label6.Name = "label6";
+			label6.Size = new Size(50, 19);
+			label6.TabIndex = 13;
+			label6.Text = "Navn:";
+			// 
+			// comboBox6
+			// 
+			comboBox6.FormattingEnabled = true;
+			comboBox6.Location = new Point(260, 365);
+			comboBox6.Name = "comboBox6";
+			comboBox6.Size = new Size(207, 23);
+			comboBox6.TabIndex = 16;
+			// 
+			// label7
+			// 
+			label7.AutoSize = true;
+			label7.Font = new Font("Gadugi", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			label7.Location = new Point(204, 365);
+			label7.Name = "label7";
+			label7.Size = new Size(50, 19);
+			label7.TabIndex = 15;
+			label7.Text = "Navn:";
+			// 
+			// CreateExaminationUserControl
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			Controls.Add(comboBox6);
+			Controls.Add(label7);
+			Controls.Add(comboBox5);
+			Controls.Add(label6);
+			Controls.Add(comboBox4);
+			Controls.Add(label4);
+			Controls.Add(comboBox3);
+			Controls.Add(label3);
+			Controls.Add(comboBox2);
+			Controls.Add(label2);
+			Controls.Add(comboBox1);
+			Controls.Add(label1);
+			Controls.Add(label5);
+			Margin = new Padding(0);
+			Name = "CreateExaminationUserControl";
+			Size = new Size(709, 580);
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Label label5;
+		private Label label1;
+		private ComboBox comboBox1;
+		private ComboBox comboBox2;
+		private Label label2;
+		private ComboBox comboBox3;
+		private Label label3;
+		private ComboBox comboBox4;
+		private Label label4;
+		private ComboBox comboBox5;
+		private Label label6;
+		private ComboBox comboBox6;
+		private Label label7;
+	}
+}
