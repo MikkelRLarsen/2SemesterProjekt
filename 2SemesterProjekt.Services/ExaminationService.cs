030035cfaa03f7602a401d5fed01581d53a74e34
AskGamKru <kruseaske@gmail.com>
2025-04-27 17:57:23 +0200

    Added ExaminationService

---
+++ 2SemesterProjekt.Services/ExaminationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _2SemesterProject.Domain.Interfaces.ServiceInterfaces;
+using _2SemesterProject.Domain.Models;
+
+namespace _2SemesterProjekt.Services
+{
+    class ExaminationService : IExaminationService
+    {
+
+        public void CreateExamination(Customer customerChoice, Pet petChoice, Examination examinationChoice, )
+        {
+
+        }
+    }
+}
