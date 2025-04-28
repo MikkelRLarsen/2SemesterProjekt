086905c81ee3d31d77ab7b100b651416bab57a83
Mikkel Schmidt Nielsen <mikkelschmidt@msn.com>
2025-04-28 10:55:18 +0200

    Made updates for validation in UI for customer

--- 2SemesterProjekt/Pages/UserControls/Customer/AddCustomer.Designer.cs
+++ 2SemesterProjekt/Pages/UserControls/Customer/AddCustomer.Designer.cs
@@ -20,196 +20,215 @@
 			base.Dispose(disposing);
 		}
 
-		#region Component Designer generated code
+        #region Component Designer generated code
 
-		/// <summary> 
-		/// Required method for Designer support - do not modify 
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			pageNameLabel = new Label();
-			labelFirstName = new Label();
-			labelLastName = new Label();
-			labelEmail = new Label();
-			labelAddress = new Label();
-			labelPhoneNumber = new Label();
-			labelType = new Label();
-			comboBoxType = new ComboBox();
-			textBoxAddress = new TextBox();
-			textBoxLastName = new TextBox();
-			textBoxFirstName = new TextBox();
-			textBoxEmail = new TextBox();
-			textBoxPhoneNumber = new TextBox();
-			panelCancel = new Panel();
-			panelCreate = new Panel();
-			SuspendLayout();
-			// 
-			// pageNameLabel
-			// 
-			pageNameLabel.AutoSize = true;
-			pageNameLabel.Font = new Font("Gadugi", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
-			pageNameLabel.Location = new Point(356, 36);
-			pageNameLabel.Name = "pageNameLabel";
-			pageNameLabel.Size = new Size(487, 77);
-			pageNameLabel.TabIndex = 0;
-			pageNameLabel.Text = "Tilføj ny kunde";
-			// 
-			// labelFirstName
-			// 
-			labelFirstName.AutoSize = true;
-			labelFirstName.Font = new Font("Gadugi", 19F, FontStyle.Bold);
-			labelFirstName.Location = new Point(174, 286);
-			labelFirstName.Name = "labelFirstName";
-			labelFirstName.Size = new Size(114, 31);
-			labelFirstName.TabIndex = 2;
-			labelFirstName.Text = "Fornavn";
-			// 
-			// labelLastName
-			// 
-			labelLastName.AutoSize = true;
-			labelLastName.Font = new Font("Gadugi", 19F, FontStyle.Bold);
-			labelLastName.Location = new Point(638, 286);
-			labelLastName.Name = "labelLastName";
-			labelLastName.Size = new Size(132, 31);
-			labelLastName.TabIndex = 3;
-			labelLastName.Text = "Efternavn";
-			// 
-			// labelEmail
-			// 
-			labelEmail.AutoSize = true;
-			labelEmail.Font = new Font("Gadugi", 19F, FontStyle.Bold);
-			labelEmail.Location = new Point(208, 383);
-			labelEmail.Name = "labelEmail";
-			labelEmail.Size = new Size(80, 31);
-			labelEmail.TabIndex = 4;
-			labelEmail.Text = "Email";
-			// 
-			// labelAddress
-			// 
-			labelAddress.AutoSize = true;
-			labelAddress.Font = new Font("Gadugi", 19F, FontStyle.Bold);
-			labelAddress.Location = new Point(638, 381);
-			labelAddress.Name = "labelAddress";
-			labelAddress.Size = new Size(108, 31);
-			labelAddress.TabIndex = 5;
-			labelAddress.Text = "Adresse";
-			// 
-			// labelPhoneNumber
-			// 
-			labelPhoneNumber.AutoSize = true;
-			labelPhoneNumber.Font = new Font("Gadugi", 19F, FontStyle.Bold);
-			labelPhoneNumber.Location = new Point(78, 476);
-			labelPhoneNumber.Name = "labelPhoneNumber";
-			labelPhoneNumber.Size = new Size(210, 31);
-			labelPhoneNumber.TabIndex = 5;
-			labelPhoneNumber.Text = "Telefonnummer";
-			// 
-			// labelType
-			// 
-			labelType.AutoSize = true;
-			labelType.Font = new Font("Gadugi", 19F, FontStyle.Bold);
-			labelType.Location = new Point(638, 476);
-			labelType.Name = "labelType";
-			labelType.Size = new Size(147, 31);
-			labelType.TabIndex = 5;
-			labelType.Text = "Kundetype";
-			// 
-			// comboBoxType
-			// 
-			comboBoxType.Font = new Font("Gadugi", 18.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-			comboBoxType.FormattingEnabled = true;
-			comboBoxType.Location = new Point(842, 476);
-			comboBoxType.Name = "comboBoxType";
-			comboBoxType.Size = new Size(240, 37);
-			comboBoxType.TabIndex = 6;
-			// 
-			// textBoxAddress
-			// 
-			textBoxAddress.Font = new Font("Gadugi", 18.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-			textBoxAddress.Location = new Point(842, 381);
-			textBoxAddress.Name = "textBoxAddress";
-			textBoxAddress.Size = new Size(230, 41);
-			textBoxAddress.TabIndex = 4;
-			// 
-			// textBoxLastName
-			// 
-			textBoxLastName.Font = new Font("Gadugi", 18.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-			textBoxLastName.Location = new Point(842, 284);
-			textBoxLastName.Name = "textBoxLastName";
-			textBoxLastName.Size = new Size(230, 41);
-			textBoxLastName.TabIndex = 2;
-			textBoxLastName.KeyPress += textBoxLastName_KeyPress;
-			// 
-			// textBoxFirstName
-			// 
-			textBoxFirstName.Font = new Font("Gadugi", 18.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-			textBoxFirstName.Location = new Point(340, 286);
-			textBoxFirstName.Name = "textBoxFirstName";
-			textBoxFirstName.Size = new Size(230, 41);
-			textBoxFirstName.TabIndex = 1;
-			textBoxFirstName.KeyPress += textBoxFirstName_KeyPress;
-			// 
-			// textBoxEmail
-			// 
-			textBoxEmail.Font = new Font("Gadugi", 18.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-			textBoxEmail.Location = new Point(340, 381);
-			textBoxEmail.Name = "textBoxEmail";
-			textBoxEmail.Size = new Size(230, 41);
-			textBoxEmail.TabIndex = 3;
-			// 
-			// textBoxPhoneNumber
-			// 
-			textBoxPhoneNumber.Font = new Font("Gadugi", 18.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-			textBoxPhoneNumber.Location = new Point(340, 474);
-			textBoxPhoneNumber.MaxLength = 8;
-			textBoxPhoneNumber.Name = "textBoxPhoneNumber";
-			textBoxPhoneNumber.Size = new Size(230, 41);
-			textBoxPhoneNumber.TabIndex = 5;
-			textBoxPhoneNumber.KeyPress += textBoxPhoneNumber_KeyPress;
-			// 
-			// panelCancel
-			// 
-			panelCancel.Location = new Point(341, 684);
-			panelCancel.Name = "panelCancel";
-			panelCancel.Size = new Size(209, 59);
-			panelCancel.TabIndex = 7;
-			// 
-			// panelCreate
-			// 
-			panelCreate.Location = new Point(650, 684);
-			panelCreate.Name = "panelCreate";
-			panelCreate.Size = new Size(209, 59);
-			panelCreate.TabIndex = 8;
-			// 
-			// AddCustomer
-			// 
-			AutoScaleDimensions = new SizeF(7F, 15F);
-			AutoScaleMode = AutoScaleMode.Font;
-			Controls.Add(panelCreate);
-			Controls.Add(panelCancel);
-			Controls.Add(comboBoxType);
-			Controls.Add(labelType);
-			Controls.Add(labelPhoneNumber);
-			Controls.Add(labelAddress);
-			Controls.Add(labelEmail);
-			Controls.Add(labelLastName);
-			Controls.Add(labelFirstName);
-			Controls.Add(textBoxPhoneNumber);
-			Controls.Add(textBoxEmail);
-			Controls.Add(textBoxFirstName);
-			Controls.Add(textBoxLastName);
-			Controls.Add(textBoxAddress);
-			Controls.Add(pageNameLabel);
-			Name = "AddCustomer";
-			Size = new Size(1200, 825);
-			ResumeLayout(false);
-			PerformLayout();
-		}
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pageNameLabel = new Label();
+            labelFirstName = new Label();
+            labelLastName = new Label();
+            labelEmail = new Label();
+            labelAddress = new Label();
+            labelPhoneNumber = new Label();
+            labelType = new Label();
+            comboBoxType = new ComboBox();
+            textBoxAddress = new TextBox();
+            textBoxLastName = new TextBox();
+            textBoxFirstName = new TextBox();
+            textBoxEmail = new TextBox();
+            textBoxPhoneNumber = new TextBox();
+            panelCancel = new Panel();
+            panelCreate = new Panel();
+            SuspendLayout();
+            // 
+            // pageNameLabel
+            // 
+            pageNameLabel.AutoSize = true;
+            pageNameLabel.Font = new Font("Gadugi", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            pageNameLabel.Location = new Point(509, 60);
+            pageNameLabel.Margin = new Padding(4, 0, 4, 0);
+            pageNameLabel.Name = "pageNameLabel";
+            pageNameLabel.Size = new Size(725, 115);
+            pageNameLabel.TabIndex = 0;
+            pageNameLabel.Text = "Tilføj ny kunde";
+            // 
+            // labelFirstName
+            // 
+            labelFirstName.AutoSize = true;
+            labelFirstName.Font = new Font("Gadugi", 19F, FontStyle.Bold);
+            labelFirstName.Location = new Point(249, 477);
+            labelFirstName.Margin = new Padding(4, 0, 4, 0);
+            labelFirstName.Name = "labelFirstName";
+            labelFirstName.Size = new Size(165, 45);
+            labelFirstName.TabIndex = 2;
+            labelFirstName.Text = "Fornavn";
+            // 
+            // labelLastName
+            // 
+            labelLastName.AutoSize = true;
+            labelLastName.Font = new Font("Gadugi", 19F, FontStyle.Bold);
+            labelLastName.Location = new Point(911, 477);
+            labelLastName.Margin = new Padding(4, 0, 4, 0);
+            labelLastName.Name = "labelLastName";
+            labelLastName.Size = new Size(193, 45);
+            labelLastName.TabIndex = 3;
+            labelLastName.Text = "Efternavn";
+            // 
+            // labelEmail
+            // 
+            labelEmail.AutoSize = true;
+            labelEmail.Font = new Font("Gadugi", 19F, FontStyle.Bold);
+            labelEmail.Location = new Point(297, 638);
+            labelEmail.Margin = new Padding(4, 0, 4, 0);
+            labelEmail.Name = "labelEmail";
+            labelEmail.Size = new Size(117, 45);
+            labelEmail.TabIndex = 4;
+            labelEmail.Text = "Email";
+            // 
+            // labelAddress
+            // 
+            labelAddress.AutoSize = true;
+            labelAddress.Font = new Font("Gadugi", 19F, FontStyle.Bold);
+            labelAddress.Location = new Point(911, 635);
+            labelAddress.Margin = new Padding(4, 0, 4, 0);
+            labelAddress.Name = "labelAddress";
+            labelAddress.Size = new Size(162, 45);
+            labelAddress.TabIndex = 5;
+            labelAddress.Text = "Adresse";
+            // 
+            // labelPhoneNumber
+            // 
+            labelPhoneNumber.AutoSize = true;
+            labelPhoneNumber.Font = new Font("Gadugi", 19F, FontStyle.Bold);
+            labelPhoneNumber.Location = new Point(111, 793);
+            labelPhoneNumber.Margin = new Padding(4, 0, 4, 0);
+            labelPhoneNumber.Name = "labelPhoneNumber";
+            labelPhoneNumber.Size = new Size(308, 45);
+            labelPhoneNumber.TabIndex = 5;
+            labelPhoneNumber.Text = "Telefonnummer";
+            // 
+            // labelType
+            // 
+            labelType.AutoSize = true;
+            labelType.Font = new Font("Gadugi", 19F, FontStyle.Bold);
+            labelType.Location = new Point(911, 793);
+            labelType.Margin = new Padding(4, 0, 4, 0);
+            labelType.Name = "labelType";
+            labelType.Size = new Size(216, 45);
+            labelType.TabIndex = 5;
+            labelType.Text = "Kundetype";
+            // 
+            // comboBoxType
+            // 
+            comboBoxType.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxType.Font = new Font("Gadugi", 18.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            comboBoxType.FormattingEnabled = true;
+            comboBoxType.Location = new Point(1203, 793);
+            comboBoxType.Margin = new Padding(4, 5, 4, 5);
+            comboBoxType.Name = "comboBoxType";
+            comboBoxType.Size = new Size(341, 53);
+            comboBoxType.TabIndex = 6;
+            // 
+            // textBoxAddress
+            // 
+            textBoxAddress.Font = new Font("Gadugi", 18.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBoxAddress.Location = new Point(1203, 635);
+            textBoxAddress.Margin = new Padding(4, 5, 4, 5);
+            textBoxAddress.Name = "textBoxAddress";
+            textBoxAddress.Size = new Size(327, 57);
+            textBoxAddress.TabIndex = 4;
+            textBoxAddress.KeyPress += textBoxAddress_KeyPress;
+            // 
+            // textBoxLastName
+            // 
+            textBoxLastName.Font = new Font("Gadugi", 18.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBoxLastName.Location = new Point(1203, 473);
+            textBoxLastName.Margin = new Padding(4, 5, 4, 5);
+            textBoxLastName.Name = "textBoxLastName";
+            textBoxLastName.Size = new Size(327, 57);
+            textBoxLastName.TabIndex = 2;
+            textBoxLastName.KeyPress += textBoxLastName_KeyPress;
+            // 
+            // textBoxFirstName
+            // 
+            textBoxFirstName.Font = new Font("Gadugi", 18.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBoxFirstName.Location = new Point(486, 477);
+            textBoxFirstName.Margin = new Padding(4, 5, 4, 5);
+            textBoxFirstName.Name = "textBoxFirstName";
+            textBoxFirstName.Size = new Size(327, 57);
+            textBoxFirstName.TabIndex = 1;
+            textBoxFirstName.KeyPress += textBoxFirstName_KeyPress;
+            // 
+            // textBoxEmail
+            // 
+            textBoxEmail.Font = new Font("Gadugi", 18.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBoxEmail.Location = new Point(486, 635);
+            textBoxEmail.Margin = new Padding(4, 5, 4, 5);
+            textBoxEmail.Name = "textBoxEmail";
+            textBoxEmail.Size = new Size(327, 57);
+            textBoxEmail.TabIndex = 3;
+            textBoxEmail.KeyPress += textBoxEmail_KeyPress;
+            // 
+            // textBoxPhoneNumber
+            // 
+            textBoxPhoneNumber.Font = new Font("Gadugi", 18.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBoxPhoneNumber.Location = new Point(486, 790);
+            textBoxPhoneNumber.Margin = new Padding(4, 5, 4, 5);
+            textBoxPhoneNumber.MaxLength = 8;
+            textBoxPhoneNumber.Name = "textBoxPhoneNumber";
+            textBoxPhoneNumber.Size = new Size(327, 57);
+            textBoxPhoneNumber.TabIndex = 5;
+            textBoxPhoneNumber.KeyPress += textBoxPhoneNumber_KeyPress;
+            // 
+            // panelCancel
+            // 
+            panelCancel.Location = new Point(487, 1140);
+            panelCancel.Margin = new Padding(4, 5, 4, 5);
+            panelCancel.Name = "panelCancel";
+            panelCancel.Size = new Size(299, 98);
+            panelCancel.TabIndex = 7;
+            // 
+            // panelCreate
+            // 
+            panelCreate.Location = new Point(929, 1140);
+            panelCreate.Margin = new Padding(4, 5, 4, 5);
+            panelCreate.Name = "panelCreate";
+            panelCreate.Size = new Size(299, 98);
+            panelCreate.TabIndex = 8;
+            // 
+            // AddCustomer
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panelCreate);
+            Controls.Add(panelCancel);
+            Controls.Add(comboBoxType);
+            Controls.Add(labelType);
+            Controls.Add(labelPhoneNumber);
+            Controls.Add(labelAddress);
+            Controls.Add(labelEmail);
+            Controls.Add(labelLastName);
+            Controls.Add(labelFirstName);
+            Controls.Add(textBoxPhoneNumber);
+            Controls.Add(textBoxEmail);
+            Controls.Add(textBoxFirstName);
+            Controls.Add(textBoxLastName);
+            Controls.Add(textBoxAddress);
+            Controls.Add(pageNameLabel);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "AddCustomer";
+            Size = new Size(1714, 1375);
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
+        #endregion
 
-		private Label pageNameLabel;
+        private Label pageNameLabel;
 		private Label labelFirstName;
 		private Label labelLastName;
 		private Label labelEmail;
--- 2SemesterProjekt/Pages/UserControls/Customer/AddCustomer.cs
+++ 2SemesterProjekt/Pages/UserControls/Customer/AddCustomer.cs
@@ -10,87 +10,158 @@ using System.Windows.Forms;
 using _2SemesterProject.Domain.Interfaces.ServiceInterfaces;
 using _2SemesterProject.Domain.Models;
 using Microsoft.Extensions.DependencyInjection;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using System.Net;
 
 namespace _2SemesterProjekt
 {
-	public partial class AddCustomer : UserControl
-	{
-		private readonly ICustomerService _customerService;
-		private readonly string[] _customerTypes = { "Privat", "Erhverv" };
+    public partial class AddCustomer : UserControl
+    {
+        private readonly ICustomerService _customerService;
+        private readonly string[] _customerTypes = { "Privat", "Erhverv" };
 
-		public AddCustomer()
-		{
-			InitializeComponent();
+        public AddCustomer()
+        {
+            InitializeComponent();
 
-			// Get the customer service from the service provider
-			_customerService = ServiceProviderSingleton.GetServiceProvider().GetService<ICustomerService>()!;
+            // Get the customer service from the service provider
+            _customerService = ServiceProviderSingleton.GetServiceProvider().GetService<ICustomerService>()!;
 
-			// Add buttons
-			panelCancel.Controls.Add(new ButtonPanel("Annuller", Color.IndianRed, CancelButton_Click));
-			panelCreate.Controls.Add(new ButtonPanel("Tilføj kunde", Color.MediumSeaGreen, CreateButton_Click));
+            // Add buttons
+            panelCancel.Controls.Add(new ButtonPanel("Annuller", Color.IndianRed, CancelButton_Click));
+            panelCreate.Controls.Add(new ButtonPanel("Tilføj kunde", Color.MediumSeaGreen, CreateButton_Click));
 
-			// Add content for dropdown
-			comboBoxType.Items.AddRange(_customerTypes);
-			comboBoxType.DropDownStyle = ComboBoxStyle.DropDownList;
-		}
+            // Add content for dropdown
+            comboBoxType.DataSource = _customerTypes;
+        }
 
-		// Eventhandler for cancel button
-		private void CancelButton_Click(object? sender, EventArgs e)
-		{
-			this.Controls.Clear();
-			this.Controls.Add(new CustomerPage());
-		}
+        // Eventhandler for cancel button
+        private void CancelButton_Click(object? sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            this.Controls.Add(new CustomerPage());
+        }
 
-		
-		private void textBoxFirstName_KeyPress(object sender, KeyPressEventArgs e)
-		{
-			// Allow only letters, control keys (backspace), and separators (space)
-			if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsSeparator(e.KeyChar))
-			{
-				e.Handled = true;
-			}
-		}
 
-		private void textBoxLastName_KeyPress(object sender, KeyPressEventArgs e)
-		{
-			// Allow only letters, control keys (backspace), and separators (space)
-			if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsSeparator(e.KeyChar))
-			{
-				e.Handled = true;
-			}
-		}
+        private void textBoxFirstName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Allow only letters, control keys (backspace), and separators (space)
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsSeparator(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            // Resets textbox after error message
+            textBoxFirstName.ForeColor = SystemColors.WindowText;
+            textBoxFirstName.BackColor = SystemColors.Window;
+        }
 
-		private void textBoxPhoneNumber_KeyPress(object sender, KeyPressEventArgs e)
-		{
-			// Allow only digits
-			if (!char.IsDigit(e.KeyChar))
-			{
-				e.Handled = true;
-			}
-		}
+        private void textBoxLastName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Allow only letters, control keys (backspace), and separators (space)
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsSeparator(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            // Resets textbox after error message
+            textBoxLastName.ForeColor = SystemColors.WindowText;
+            textBoxLastName.BackColor = SystemColors.Window;
+        }
 
-		private void CreateButton_Click(object? sender, EventArgs e)
-		{
+        private void textBoxEmail_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Resets textbox after error message
+            textBoxEmail.ForeColor = SystemColors.WindowText;
+            textBoxEmail.BackColor = SystemColors.Window;
+        }
 
-			var customer = new Customer(
-					textBoxFirstName.Text, // FirstName
-					textBoxLastName.Text,  // LastName
-					textBoxEmail.Text,     // Email
-					textBoxAddress.Text,   // Address
-					comboBoxType.Text,     // Type
-					Convert.ToInt32(textBoxPhoneNumber.Text) // PhoneNumber
-				);
+        private void textBoxAddress_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Resets textbox after error message
+            textBoxAddress.ForeColor = SystemColors.WindowText;
+            textBoxAddress.BackColor = SystemColors.Window;
+        }
 
-			var result = _customerService.CreateCustomer(customer);
+        private void textBoxPhoneNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Allow only digits
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            // Resets textbox after error message
+            textBoxPhoneNumber.ForeColor = SystemColors.WindowText;
+            textBoxPhoneNumber.BackColor = SystemColors.Window;
+        }
 
-			if (result == true) // Display success result
-			{
-				MessageBox.Show($"{customer.FirstName} {customer.LastName} er blevet oprettet");
-			}
-			else // Display failure message
-			{
-				MessageBox.Show($"Fejl under oprettelse");
-			}
-		}
-	}
+        private void CreateButton_Click(object? sender, EventArgs e)
+        {
+            string displayMessage = string.Empty;
+
+            // Validate first and last name: only letters
+            if (string.IsNullOrWhiteSpace(textBoxFirstName.Text) || !textBoxFirstName.Text.All(char.IsLetter))
+            {
+                textBoxFirstName.ForeColor = Color.White;
+                textBoxFirstName.BackColor = Color.LightCoral;
+                displayMessage += "Fornavn må kun indeholde bogstaver.\n";
+            }
+
+            if (string.IsNullOrWhiteSpace(textBoxLastName.Text) || !textBoxLastName.Text.All(char.IsLetter))
+            {
+                textBoxLastName.ForeColor = Color.White;
+                textBoxLastName.BackColor = Color.LightCoral;
+                displayMessage += "Efternavn må kun indeholde bogstaver.\n";
+            }
+
+            // Validate email: "@" and "." in correct order.
+            int atIndex = textBoxEmail.Text.IndexOf('@');
+            int dotIndex = textBoxEmail.Text.LastIndexOf('.');
+            if (string.IsNullOrWhiteSpace(textBoxEmail.Text) || atIndex <= 0 || dotIndex <= atIndex + 1 || dotIndex == textBoxEmail.Text.Length - 1)
+            {
+                textBoxEmail.ForeColor = Color.White;
+                textBoxEmail.BackColor = Color.LightCoral;
+                displayMessage += "Indtast en gyldig e-mailadresse.\n";
+            }
+
+            // Validate address: only letters and digits.
+            if (string.IsNullOrWhiteSpace(textBoxAddress.Text) || textBoxAddress.Text.All(char.IsLetterOrDigit))
+            {
+                textBoxAddress.ForeColor = Color.White;
+                textBoxAddress.BackColor = Color.LightCoral;
+                displayMessage += "Adressen må kun indeholde bogstaver, tal og mellemrum.\n";
+            }
+
+            // Validate phonenumber: 8-digit long.
+            int customerPhoneNumber = 0;
+            if (textBoxPhoneNumber.Text != string.Empty)
+            {
+                customerPhoneNumber = Convert.ToInt32(textBoxPhoneNumber.Text);
+            }
+
+            if (customerPhoneNumber < 10000000 || customerPhoneNumber > 99999999)
+            {
+                textBoxPhoneNumber.ForeColor = Color.White;
+                textBoxPhoneNumber.BackColor = Color.LightCoral;
+                displayMessage += "Telefonnummer skal være et 8-cifret tal.\n";
+            }
+
+            if (displayMessage == string.Empty) // If there is no errors, then create customer
+            {
+                var customer = new Customer(
+                    textBoxFirstName.Text, // FirstName
+                    textBoxLastName.Text,  // LastName
+                    textBoxEmail.Text,     // Email
+                    textBoxAddress.Text,   // Address
+                    comboBoxType.Text,     // Type
+                    Convert.ToInt32(textBoxPhoneNumber.Text) // PhoneNumber
+            );
+
+                _customerService.CreateCustomer(customer);
+                MessageBox.Show($"{customer.FirstName} er oprettet i systemet");
+            }
+            else // Show error to user
+            {
+                MessageBox.Show(displayMessage, "Fejl i indtastning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
 }
