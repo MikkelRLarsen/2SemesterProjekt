ac585485e414087a60a95b7634bfb764bbeb85dc
Sarah <peasblinky@gmail.com>
2025-04-28 15:39:19 +0200

    MS Test for IPetService - work in progress.

---
+++ 2SemesterProjekt.MSTest/Services.Test/IPetServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _2SemesterProject.Domain.Interfaces;
+namespace _2SemesterProjekt.MSTest;
+
+[TestClass]
+public class IPetServiceTest
+{
+    [TestMethod]
+    public bool ReturnTrueIfPetAlreadyExists()
+    {
+    }
+
+    [TestMethod]
+    public bool ReturnTrueIfCustomerDoesntExist()
+    {
+    }
+
+    [TestMethod]
+    public bool ReturnTrueIfNameOrSpeciesIsInvalid()
+    {
+    }
+}
