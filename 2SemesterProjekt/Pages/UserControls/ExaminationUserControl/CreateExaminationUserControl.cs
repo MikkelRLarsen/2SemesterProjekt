6f0e3ff5cb2cd9bcb73d192fdccfc0cd9b67e29f
MikkelRLarsen <Thenuker95@gmail.com>
2025-04-29 14:04:31 +0200

    Added comments to CreateExamination

--- 2SemesterProjekt/Pages/UserControls/ExaminationUserControl/CreateExaminationUserControl.cs
+++ 2SemesterProjekt/Pages/UserControls/ExaminationUserControl/CreateExaminationUserControl.cs
@@ -1,4 +1,4 @@
-﻿using _2SemesterProject.Domain.Interfaces.ServiceInterfaces;
+﻿using _2SemesterProjekt.Domain.Interfaces.ServiceInterfaces;
 using _2SemesterProjekt.Domain.Models;
 using _2SemesterProjekt.Domain.Interfaces.ServiceInterfaces;
 using Microsoft.Extensions.DependencyInjection;
@@ -35,30 +35,55 @@ namespace _2SemesterProjekt.Pages.UserControls.ExaminationUserControl
 			}
 		}
 
+		/// <summary>
+		/// Eventhandler for when PetExaminationDropdown Index is changed
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private async void PetExaminationDropdown_SelectedIndexChanged(object sender, EventArgs e)
 		{
 			ExaminationDropdown.DataSource = await _examinationService.GetAllExaminationTypesAsync();
 			ExaminationDropdown.Enabled = true;
 		}
 
+		/// <summary>
+		/// Eventhandler for when ExaminationDropdown Index is changed
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private async void ExaminationDropdown_SelectedIndexChanged(object sender, EventArgs e)
 		{
 			PriceExaminationDisplay.Text = Convert.ToString(_examinationService.GetAllExaminationPrices(ExaminationDropdown.SelectedItem as string));
 			DateTimePickerExamination.Enabled = true;
 		}
 
+		/// <summary>
+		/// Eventhandler for when DateTimePickerExamination Value is Changed
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private async void DateTimePickerExamination_ValueChanged(object sender, EventArgs e)
 		{
 			EmployeeExaminationDropdown.Enabled = true;
 			EmployeeExaminationDropdown.DataSource = await _employeeService.GetAllEmployeeAsync();
 		}
 
+		/// <summary>
+		/// Eventhandler for when EmployeeExaminationDropdown is changed
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void EmployeeExaminationDropdown_SelectedIndexChanged(object sender, EventArgs e)
 		{
 			CreateExaminationButton.Enabled = true;
 			CreateExaminationButton.BackColor = Color.MediumAquamarine;
 		}
 
+		/// <summary>
+		/// Button to create Examination. Currently Not-Async
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void CreateExaminationButton_Click(object sender, EventArgs e)
 		{
 
@@ -82,6 +107,11 @@ namespace _2SemesterProjekt.Pages.UserControls.ExaminationUserControl
 			}
 		}
 
+		/// <summary>
+		/// Happens when the form is Loaded, not constructed.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private async void CreateExaminationUserControl_Load(object sender, EventArgs e)
 		{
 			ServiceProvider allServices = ServiceProviderSingleton.GetServiceProvider();
@@ -97,6 +127,9 @@ namespace _2SemesterProjekt.Pages.UserControls.ExaminationUserControl
 			CustomerExaminationDropdown.DataSource = await _customerService.GetAllCustomersAsync();
 		}
 
+		/// <summary>
+		/// Set DisplayMembers foreach Combobox/Dropdown
+		/// </summary>
 		private void SetAllDisplayMembers()
 		{
 			CustomerExaminationDropdown.DisplayMember = "FirstName";
@@ -104,11 +137,21 @@ namespace _2SemesterProjekt.Pages.UserControls.ExaminationUserControl
 			EmployeeExaminationDropdown.DisplayMember = "FirstName";
 		}
 
+		/// <summary>
+		/// Returns user back to prior page
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void CancelExaminationButton_Click(object sender, EventArgs e)
 		{
 			_konsultationPanel.Controls.Clear();
 		}
 
+		/// <summary>
+		/// Sets PriceExaminationsDisplay to white, when the value has changed. Default is greyed out
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void PriceExaminationDisplay_TextChanged(object sender, EventArgs e)
 		{
 			PriceExaminationDisplay.BackColor = Color.White;
