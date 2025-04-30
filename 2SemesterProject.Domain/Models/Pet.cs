69e539ed098dababcd0dbe92a9511a2a2afbeec6
Mikkel Schmidt Nielsen <mikkelschmidt@msn.com>
2025-04-30 12:49:27 +0200

    Updated the Pet model

--- 2SemesterProject.Domain/Models/Pet.cs
+++ 2SemesterProject.Domain/Models/Pet.cs
@@ -4,11 +4,13 @@
 	{
 		public int PetID { get; init; }
 		public int CustomerID { get; protected set; }
+        public int? EmployeeID { get; protected set; }
 		public string Name { get; protected set; }
 		public string Species { get; protected set; }
 		public DateTime	Birthday { get; protected set; }
 
 		public Customer Customer { get; }
+        public Employee? Employee { get; }
 		public List<Examination> Examinations { get; }
 
 		public Pet(int petID, int customerID, string name, string species, DateTime birthday)
@@ -19,12 +21,13 @@
 			Species = species;
 			Birthday = birthday;
 		}
-        public Pet(int customerID, string name, string species, DateTime birthday)
+        public Pet(int customerID, string name, string species, DateTime birthday, int? employeeID)
         {
             CustomerID = customerID;
             Name = name;
             Species = species;
             Birthday = birthday;
+            EmployeeID = employeeID;
 
             if (ValidateInformation() == false)
             {
