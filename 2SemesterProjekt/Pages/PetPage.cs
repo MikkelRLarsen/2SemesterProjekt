d3d92b4c672e48dbcd674d77079b94e31b4200af
MikkelSchmidtNielsen <mikkelschmidt@msn.com>
2025-05-05 19:02:57 +0200

    Made UI not load entire pet list each time

--- 2SemesterProjekt/Pages/PetPage.cs
+++ 2SemesterProjekt/Pages/PetPage.cs
@@ -34,8 +34,9 @@ namespace _2SemesterProjekt.Pages
         }
         private void ShowPetCreation(object sender, EventArgs e)
         {
-            this.Controls.Clear(); // Clear existing content
-            this.Controls.Add(new AddPetUserControl()); // Load the new page
+            var addPetControl = new AddPetUserControl();
+            this.Controls.Add(addPetControl); // Load the new page
+			addPetControl.BringToFront();
         }
 
         private void ShowPetUpdate(object sender, EventArgs e)
@@ -43,8 +44,8 @@ namespace _2SemesterProjekt.Pages
             if (this.PetCard != null)
             {
                 var updatePetControl = new UpdatePetUserControl(PetCard);
-				this.Controls.Add(updatePetControl);
-                updatePetControl.BringToFront();
+				this.Controls.Add(updatePetControl); // Load the new page
+				updatePetControl.BringToFront();
             }
             else
             {
@@ -93,6 +94,8 @@ namespace _2SemesterProjekt.Pages
                     flowLayoutPanel1.Controls.Add(new PetCard(this, pet));
                 }
 
+                // Checks if the button already exists before adding
+                if (!buttonFlowPanel.Controls.OfType<ButtonPanel>().Any(b => b.Text == "Vis alle kæledyr"))
                 // Add option for resetting the flowpanel
                 buttonFlowPanel.Controls.Add(new ButtonPanel("Vis alle kæledyr", Color.MediumVioletRed, ShowAllPets));
             }
