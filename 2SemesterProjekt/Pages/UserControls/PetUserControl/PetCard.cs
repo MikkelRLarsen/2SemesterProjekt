3180f225f777188e2bfe46c0c523bf7814ed7702
AskGamKru <kruseaske@gmail.com>
2025-05-05 13:50:14 +0200

    Revert "added implementation of ExaminationService on PetPage"
    This reverts commit cc4085423d67fc0b558d6f7ccec0d534b051cb01.

--- 2SemesterProjekt/Pages/UserControls/PetUserControl/PetCard.cs
+++ 2SemesterProjekt/Pages/UserControls/PetUserControl/PetCard.cs
@@ -24,7 +24,7 @@ namespace _2SemesterProjekt.Pages.UserControls.PetUserControl
     {
         private PetPage _petPage;
         private PetCardType _petCardType;
-        private IExaminationService examinationService;
+        private IPetService _petService;
         public Pet Pet { get; }
         public Examination Examination { get; }
         
