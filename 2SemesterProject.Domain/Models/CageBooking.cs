a95f3d6e4f9ad4c35974fcced6a0585803423e7b
Mikkel Schmidt Nielsen <mikkelschmidt@msn.com>
2025-05-07 12:55:52 +0200

    Made models for Cage and CageBooking

---
+++ 2SemesterProject.Domain/Models/CageBooking.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _2SemesterProjekt.Domain.Models;
+
+namespace _2SemesterProject.Domain.Models
+{
+    public class CageBooking
+    {
+        public int CageBookingID { get; init; }
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+        public decimal TotalPrice { get; private set; }
+        public Examination Examination { get; }
+        public Cage Cage { get; }
+
+        public CageBooking(int cageBookingID, DateTime startDate, DateTime endDate, decimal totalPrice)
+        {
+            CageBookingID = cageBookingID;
+            StartDate = startDate;
+            EndDate = endDate;
+            TotalPrice = totalPrice;
+        }
+    }
+}
