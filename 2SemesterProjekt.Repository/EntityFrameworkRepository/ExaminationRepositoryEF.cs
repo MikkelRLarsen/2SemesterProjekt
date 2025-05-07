97a8cc7c6dcbd8254f7e74a3dfce58fc79a1319f
AskGamKru <kruseaske@gmail.com>
2025-05-07 13:34:03 +0200

    fixed comments

--- 2SemesterProjekt.Repository/EntityFrameworkRepository/ExaminationRepositoryEF.cs
+++ 2SemesterProjekt.Repository/EntityFrameworkRepository/ExaminationRepositoryEF.cs
@@ -79,21 +79,21 @@ namespace _2SemesterProjekt.Repository.EntityFrameworkRepository
 			var twelveMonthsAgo = DateTime.Now.AddMonths(-12);
 
             // This is a subquery inside a query
-            // First it find all examination which is older than 12 months AND where a pet haven't had a examination within the last 12 months
-            // Then orders them by PetID and afterwards order them by examination date, so I can filter through them in BLL
+            // First it finds all examinations which are older than 12 months AND where a pet haven't had an examination within the last 12 months.
+            // Then orders them by PetID and afterwards order them by examination date, so I can filter through them in BLL.
 
 			var result = await _db.Examinations
 				.Where(e => e.Date < twelveMonthsAgo &&
 
 							!_db.Examinations // Subquery begins
 								.Where(ex => ex.Date >= twelveMonthsAgo) // Find all examinations within 12 months
-								.Select(ex => ex.PetID) // Selects the PetID, since its the only propperty im intereset in
-								.Distinct() // I dont want duplitcated petID, so I only want Distinct of them. 
-								.Contains(e.PetID)) // Checks if the PetID from subquery is inside the original search
-								// Summery of subquery. Gets all Distinct PetID from examinations which have happend wihtin 12 months
-                                // and checks if they were in the original seach which contains all examination older than 12 months
+								.Select(ex => ex.PetID) // Selects the PetID, since its the only propperty I'm interested in.
+								.Distinct() // I dont want duplicated petID, only the Distinct PetID. 
+								.Contains(e.PetID)) // Checks if the PetID from subquery is inside the original search.
+								// Summery of subquery. Gets all Distinct PetID from examinations which have happened within 12 months.
+                                // and checks if they were in the original search which contains all examination older than 12 months.
 
-                // All in all: query returns all examinations that are older than 12 months and where the pet haven't had a examination within 12 months
+                // All in all: query returns all examinations that are older than 12 months and where the pet haven't had an examination within 12 months.
 				.Include(e => e.Pet)
 					.ThenInclude(p => p.Customer)
 				.OrderBy(e => e.PetID)
