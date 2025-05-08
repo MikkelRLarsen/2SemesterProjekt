4c12f27104a7b5bbc9f9e0d61e637e2855e214ce
AskGamKru <kruseaske@gmail.com>
2025-05-08 19:44:47 +0200

    Added model, no validation

---
+++ 2SemesterProject.Domain/Models/Medicine.cs
@@ -0,0 +1,30 @@
+﻿using _2SemesterProjekt.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2SemesterProject.Domain.Models
+{
+    public class Medicine
+    {
+        public int MedicineID { get; init; }
+        public string Name { get; private set; }
+        public string Type { get; private set; }
+        public int DoseMiligram { get; private set; }
+        public int DosesTake24Hours { get; private set; }
+        public int DoseDurationDays { get; private set; }
+        public List<Examination> Examinations { get; }
+
+        public Medicine(string name, string type, int doseMiligram, int dosesTake24Hours, int doseDurationDays)
+        {
+            Name = name;
+            Type = type;
+            DoseMiligram = doseMiligram;
+            DosesTake24Hours = dosesTake24Hours;
+            DoseDurationDays = doseDurationDays;
+        }
+    }
+
+}
