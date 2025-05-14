183cb2886b23237871f7327d34ec3ba21f645968
AskGamKru <kruseaske@gmail.com>
2025-05-14 11:31:41 +0200

    added "Find Alle" knap

--- 2SemesterProjekt/Pages/CustomerPage.cs
+++ 2SemesterProjekt/Pages/CustomerPage.cs
@@ -17,11 +17,17 @@ namespace _2SemesterProjekt
 			InitializeComponent();
 
 			// Button with a click event handler
+			buttonFlowPanel.Controls.Add(new ButtonPanel("Vis alle", "", Color.MediumAquamarine, ShowAllCustomersButton_Click));
 			buttonFlowPanel.Controls.Add(new ButtonPanel("Tilføj kunde", "AddCustomer.png", Color.MediumSeaGreen, AddCustomerButton_Click));
 		}
+		// Eventhandler with "Show Áll Customers" when button is clicked
+		private void ShowAllCustomersButton_Click(object? sender, EventArgs e)
+		{
+            this.Controls.Clear();
+        }
 
-		// Event handler with "Add Customer" when button is clicked
-		private void AddCustomerButton_Click(object? sender, EventArgs e)
+        // Event handler with "Add Customer" when button is clicked
+        private void AddCustomerButton_Click(object? sender, EventArgs e)
 		{
 			this.Controls.Clear();
 			this.Controls.Add(new AddCustomer());
