f9366ab6286add2b1f8aa87fba60f3ea3bfc1772
MikkelRLarsen <Thenuker95@gmail.com>
2025-05-16 13:37:17 +0200

    Added Move Funktion

--- 2SemesterProjekt/Pages/Page.Designer.cs
+++ 2SemesterProjekt/Pages/Page.Designer.cs
@@ -20,27 +20,31 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            SuspendLayout();
-            // 
-            // Page
-            // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(2500, 1650);
-            FormBorderStyle = FormBorderStyle.None;
-            Name = "Page";
-            Text = "Page";
-            ResumeLayout(false);
-        }
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			SuspendLayout();
+			// 
+			// Page
+			// 
+			AutoScaleDimensions = new SizeF(10F, 25F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(2906, 1826);
+			FormBorderStyle = FormBorderStyle.None;
+			Margin = new Padding(4, 4, 4, 4);
+			Name = "Page";
+			Text = "Page";
+			MouseDown += Page_MouseDown;
+			MouseMove += Page_MouseMove;
+			MouseUp += Page_MouseUp;
+			ResumeLayout(false);
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
--- 2SemesterProjekt/Pages/Page.cs
+++ 2SemesterProjekt/Pages/Page.cs
@@ -10,11 +10,38 @@ using System.Windows.Forms;
 
 namespace _2SemesterProjekt.Pages
 {
-    public partial class Page : Form
-    {
-        public Page()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class Page : Form
+	{
+		private bool mouseDown;
+		private Point lastLocation;
+
+		public Page()
+		{
+			InitializeComponent();
+		}
+
+		private void Page_MouseDown(object sender, MouseEventArgs e)
+		{
+			mouseDown = true;
+			lastLocation = e.Location;
+
+		}
+
+		private void Page_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (mouseDown)
+			{
+				this.Location = new Point(
+					(this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+				this.Update();
+			}
+
+		}
+
+		private void Page_MouseUp(object sender, MouseEventArgs e)
+		{
+			mouseDown = false;
+		}
+	}
 }
