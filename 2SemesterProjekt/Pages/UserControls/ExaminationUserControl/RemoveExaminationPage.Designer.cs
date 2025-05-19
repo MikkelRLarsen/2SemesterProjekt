d1abc47870109a935b5ff43886ac7bbdb6548695
Mikkel Schmidt Nielsen <mikkelschmidt@msn.com>
2025-05-19 17:20:26 +0200

    DeleteExa made

--- 2SemesterProjekt/Pages/UserControls/ExaminationUserControl/RemoveExaminationPage.Designer.cs
+++ 2SemesterProjekt/Pages/UserControls/ExaminationUserControl/RemoveExaminationPage.Designer.cs
@@ -50,20 +50,21 @@
             // 
             label2.AutoSize = true;
             label2.BackColor = Color.Linen;
-            label2.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label2.ForeColor = Color.SaddleBrown;
-            label2.Location = new Point(739, 108);
+            label2.Location = new Point(429, 37);
             label2.Name = "label2";
-            label2.Size = new Size(1023, 81);
+            label2.Size = new Size(625, 50);
             label2.TabIndex = 26;
             label2.Text = "S L E T   K O N S U L T A T I O N E R";
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.PageTitelBackground;
-            pictureBox1.Location = new Point(84, 77);
+            pictureBox1.Location = new Point(101, 20);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(2332, 144);
+            pictureBox1.Size = new Size(1399, 86);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 27;
             pictureBox1.TabStop = false;
             // 
@@ -72,29 +73,30 @@
             flowPanel.AutoScroll = true;
             flowPanel.BackColor = Color.Linen;
             flowPanel.BorderStyle = BorderStyle.FixedSingle;
-            flowPanel.Location = new Point(609, 452);
+            flowPanel.Location = new Point(416, 243);
             flowPanel.Name = "flowPanel";
-            flowPanel.Size = new Size(1282, 867);
+            flowPanel.Size = new Size(769, 562);
             flowPanel.TabIndex = 42;
             // 
             // textBoxCustomerPhoneNumberOrName
             // 
-            textBoxCustomerPhoneNumberOrName.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBoxCustomerPhoneNumberOrName.Location = new Point(845, 355);
+            textBoxCustomerPhoneNumberOrName.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBoxCustomerPhoneNumberOrName.Location = new Point(570, 194);
             textBoxCustomerPhoneNumberOrName.Margin = new Padding(3, 4, 3, 4);
             textBoxCustomerPhoneNumberOrName.MaxLength = 8;
             textBoxCustomerPhoneNumberOrName.Name = "textBoxCustomerPhoneNumberOrName";
-            textBoxCustomerPhoneNumberOrName.Size = new Size(457, 42);
+            textBoxCustomerPhoneNumberOrName.Size = new Size(274, 27);
             textBoxCustomerPhoneNumberOrName.TabIndex = 41;
             // 
             // customerSearchButton
             // 
             customerSearchButton.BackColor = Color.AntiqueWhite;
             customerSearchButton.Image = Properties.Resources.SearchButton;
-            customerSearchButton.Location = new Point(1323, 359);
+            customerSearchButton.Location = new Point(850, 197);
             customerSearchButton.Margin = new Padding(3, 4, 3, 4);
             customerSearchButton.Name = "customerSearchButton";
-            customerSearchButton.Size = new Size(79, 35);
+            customerSearchButton.Size = new Size(47, 21);
+            customerSearchButton.SizeMode = PictureBoxSizeMode.StretchImage;
             customerSearchButton.TabIndex = 40;
             customerSearchButton.TabStop = false;
             customerSearchButton.Click += customerSearchButton_Click;
@@ -102,9 +104,10 @@
             // findAllButton
             // 
             findAllButton.Image = Properties.Resources.FindAllButton;
-            findAllButton.Location = new Point(1490, 359);
+            findAllButton.Location = new Point(931, 197);
             findAllButton.Name = "findAllButton";
-            findAllButton.Size = new Size(165, 35);
+            findAllButton.Size = new Size(99, 21);
+            findAllButton.SizeMode = PictureBoxSizeMode.StretchImage;
             findAllButton.TabIndex = 39;
             findAllButton.TabStop = false;
             findAllButton.Click += findAllButton_Click;
@@ -113,11 +116,11 @@
             // 
             label1.AutoSize = true;
             label1.BackColor = Color.AntiqueWhite;
-            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.SaddleBrown;
-            label1.Location = new Point(771, 307);
+            label1.Location = new Point(483, 169);
             label1.Name = "label1";
-            label1.Size = new Size(958, 31);
+            label1.Size = new Size(635, 20);
             label1.TabIndex = 38;
             label1.Text = "T R Y K   F I N D   A L L E   E L L E R   S Ø G   P Å   K U N D E N S   T E L E F O N N U M M E R";
             // 
@@ -125,28 +128,31 @@
             // 
             pictureBox3.BackColor = Color.Linen;
             pictureBox3.Image = Properties.Resources.PageBox;
-            pictureBox3.Location = new Point(609, 277);
+            pictureBox3.Location = new Point(416, 149);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(1282, 148);
+            pictureBox3.Size = new Size(769, 88);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox3.TabIndex = 37;
             pictureBox3.TabStop = false;
             // 
             // pictureBox4
             // 
             pictureBox4.Image = Properties.Resources.PageBackground;
-            pictureBox4.Location = new Point(84, 242);
+            pictureBox4.Location = new Point(101, 125);
             pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(2332, 1116);
+            pictureBox4.Size = new Size(1399, 705);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox4.TabIndex = 43;
             pictureBox4.TabStop = false;
             // 
             // deleteButton
             // 
             deleteButton.Image = Properties.Resources.DeleteButtonGreyedOut;
-            deleteButton.Location = new Point(1691, 1422);
+            deleteButton.Location = new Point(1221, 750);
             deleteButton.Margin = new Padding(3, 4, 3, 4);
             deleteButton.Name = "deleteButton";
-            deleteButton.Size = new Size(400, 92);
+            deleteButton.Size = new Size(240, 55);
+            deleteButton.SizeMode = PictureBoxSizeMode.StretchImage;
             deleteButton.TabIndex = 44;
             deleteButton.TabStop = false;
             deleteButton.Click += deleteButton_Click;
@@ -166,7 +172,7 @@
             Controls.Add(label2);
             Controls.Add(pictureBox1);
             Name = "RemoveExaminationPage";
-            Size = new Size(2500, 1580);
+            Size = new Size(1600, 845);
             Load += RemoveExaminationPage_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)customerSearchButton).EndInit();
