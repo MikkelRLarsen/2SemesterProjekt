c66dc00f2eb0ea3ee625a9ba83a7208b3f93953b
Sarah <peasblinky@gmail.com>
2025-05-19 17:25:29 +0200

    Fixed CreateCustomer

--- 2SemesterProjekt/Pages/UserControls/CustomerUserControl/CreateCustomerUpdate.Designer.cs
+++ 2SemesterProjekt/Pages/UserControls/CustomerUserControl/CreateCustomerUpdate.Designer.cs
@@ -71,9 +71,9 @@
             label6.BackColor = Color.AntiqueWhite;
             label6.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label6.ForeColor = Color.SaddleBrown;
-            label6.Location = new Point(708, 647);
+            label6.Location = new Point(809, 863);
             label6.Name = "label6";
-            label6.Size = new Size(239, 25);
+            label6.Size = new Size(288, 31);
             label6.TabIndex = 32;
             label6.Text = "T E L E F O N N U M M E R";
             // 
@@ -83,9 +83,9 @@
             label7.BackColor = Color.AntiqueWhite;
             label7.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label7.ForeColor = Color.SaddleBrown;
-            label7.Location = new Point(708, 721);
+            label7.Location = new Point(809, 961);
             label7.Name = "label7";
-            label7.Size = new Size(162, 25);
+            label7.Size = new Size(194, 31);
             label7.TabIndex = 33;
             label7.Text = "K U N D E T Y P E";
             // 
@@ -95,9 +95,9 @@
             label1.BackColor = Color.AntiqueWhite;
             label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label1.ForeColor = Color.SaddleBrown;
-            label1.Location = new Point(649, 427);
+            label1.Location = new Point(742, 569);
             label1.Name = "label1";
-            label1.Size = new Size(103, 15);
+            label1.Size = new Size(135, 20);
             label1.TabIndex = 51;
             label1.Text = "T E L E F O N N R .";
             // 
@@ -107,9 +107,9 @@
             label5.BackColor = Color.AntiqueWhite;
             label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label5.ForeColor = Color.SaddleBrown;
-            label5.Location = new Point(662, 352);
+            label5.Location = new Point(757, 469);
             label5.Name = "label5";
-            label5.Size = new Size(76, 15);
+            label5.Size = new Size(97, 20);
             label5.TabIndex = 50;
             label5.Text = "A D R E S S E";
             // 
@@ -119,9 +119,9 @@
             label4.BackColor = Color.AntiqueWhite;
             label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label4.ForeColor = Color.SaddleBrown;
-            label4.Location = new Point(637, 277);
+            label4.Location = new Point(728, 369);
             label4.Name = "label4";
-            label4.Size = new Size(126, 15);
+            label4.Size = new Size(163, 20);
             label4.TabIndex = 49;
             label4.Text = "E M A I L A D R E S S E";
             // 
@@ -131,9 +131,9 @@
             label3.BackColor = Color.AntiqueWhite;
             label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label3.ForeColor = Color.SaddleBrown;
-            label3.Location = new Point(651, 202);
+            label3.Location = new Point(744, 269);
             label3.Name = "label3";
-            label3.Size = new Size(98, 15);
+            label3.Size = new Size(129, 20);
             label3.TabIndex = 48;
             label3.Text = "E F T E R N A V N";
             // 
@@ -143,38 +143,42 @@
             label2.BackColor = Color.AntiqueWhite;
             label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label2.ForeColor = Color.SaddleBrown;
-            label2.Location = new Point(659, 127);
+            label2.Location = new Point(753, 169);
             label2.Name = "label2";
-            label2.Size = new Size(82, 15);
+            label2.Size = new Size(107, 20);
             label2.TabIndex = 44;
             label2.Text = "F O R N A V N";
             // 
             // textBoxFirstName
             // 
             textBoxFirstName.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBoxFirstName.Location = new Point(580, 146);
-            textBoxFirstName.MaxLength = 8;
+            textBoxFirstName.Location = new Point(663, 195);
+            textBoxFirstName.Margin = new Padding(3, 4, 3, 4);
+            textBoxFirstName.MaxLength = 0;
             textBoxFirstName.Name = "textBoxFirstName";
-            textBoxFirstName.Size = new Size(240, 23);
+            textBoxFirstName.Size = new Size(274, 27);
             textBoxFirstName.TabIndex = 43;
             // 
             // submitButton
             // 
             submitButton.Enabled = false;
             submitButton.Image = Properties.Resources.SaveButtonGreyedOut;
-            submitButton.Location = new Point(827, 569);
+            submitButton.Location = new Point(945, 759);
+            submitButton.Margin = new Padding(3, 4, 3, 4);
             submitButton.Name = "submitButton";
-            submitButton.Size = new Size(210, 41);
+            submitButton.Size = new Size(240, 55);
             submitButton.SizeMode = PictureBoxSizeMode.StretchImage;
             submitButton.TabIndex = 41;
             submitButton.TabStop = false;
+            submitButton.Click += submitButton_Click;
             // 
             // cancelButton
             // 
             cancelButton.Image = Properties.Resources.CancelButton;
-            cancelButton.Location = new Point(364, 569);
+            cancelButton.Location = new Point(416, 759);
+            cancelButton.Margin = new Padding(3, 4, 3, 4);
             cancelButton.Name = "cancelButton";
-            cancelButton.Size = new Size(210, 41);
+            cancelButton.Size = new Size(240, 55);
             cancelButton.SizeMode = PictureBoxSizeMode.StretchImage;
             cancelButton.TabIndex = 40;
             cancelButton.TabStop = false;
@@ -183,10 +187,9 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.PageTitelBackground;
-            pictureBox1.Location = new Point(88, 15);
-            pictureBox1.Margin = new Padding(3, 2, 3, 2);
+            pictureBox1.Location = new Point(101, 20);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(1224, 64);
+            pictureBox1.Size = new Size(1399, 85);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 45;
             pictureBox1.TabStop = false;
@@ -195,10 +198,9 @@
             // 
             pictureBox3.BackColor = Color.Linen;
             pictureBox3.Image = Properties.Resources.PageBox;
-            pictureBox3.Location = new Point(364, 112);
-            pictureBox3.Margin = new Padding(3, 2, 3, 2);
+            pictureBox3.Location = new Point(416, 149);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(673, 66);
+            pictureBox3.Size = new Size(769, 88);
             pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox3.TabIndex = 47;
             pictureBox3.TabStop = false;
@@ -207,10 +209,9 @@
             // 
             pictureBox9.BackColor = Color.Linen;
             pictureBox9.Image = Properties.Resources.PageBox;
-            pictureBox9.Location = new Point(364, 187);
-            pictureBox9.Margin = new Padding(3, 2, 3, 2);
+            pictureBox9.Location = new Point(416, 249);
             pictureBox9.Name = "pictureBox9";
-            pictureBox9.Size = new Size(673, 66);
+            pictureBox9.Size = new Size(769, 88);
             pictureBox9.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox9.TabIndex = 52;
             pictureBox9.TabStop = false;
@@ -219,10 +220,9 @@
             // 
             pictureBox4.BackColor = Color.Linen;
             pictureBox4.Image = Properties.Resources.PageBox;
-            pictureBox4.Location = new Point(364, 262);
-            pictureBox4.Margin = new Padding(3, 2, 3, 2);
+            pictureBox4.Location = new Point(416, 349);
             pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(673, 66);
+            pictureBox4.Size = new Size(769, 88);
             pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox4.TabIndex = 53;
             pictureBox4.TabStop = false;
@@ -231,10 +231,9 @@
             // 
             pictureBox5.BackColor = Color.Linen;
             pictureBox5.Image = Properties.Resources.PageBox;
-            pictureBox5.Location = new Point(364, 337);
-            pictureBox5.Margin = new Padding(3, 2, 3, 2);
+            pictureBox5.Location = new Point(416, 449);
             pictureBox5.Name = "pictureBox5";
-            pictureBox5.Size = new Size(673, 66);
+            pictureBox5.Size = new Size(769, 88);
             pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox5.TabIndex = 54;
             pictureBox5.TabStop = false;
@@ -243,10 +242,9 @@
             // 
             pictureBox6.BackColor = Color.Linen;
             pictureBox6.Image = Properties.Resources.PageBox;
-            pictureBox6.Location = new Point(364, 412);
-            pictureBox6.Margin = new Padding(3, 2, 3, 2);
+            pictureBox6.Location = new Point(416, 549);
             pictureBox6.Name = "pictureBox6";
-            pictureBox6.Size = new Size(673, 66);
+            pictureBox6.Size = new Size(769, 88);
             pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox6.TabIndex = 55;
             pictureBox6.TabStop = false;
@@ -254,10 +252,9 @@
             // pictureBox2
             // 
             pictureBox2.Image = Properties.Resources.PageBackground;
-            pictureBox2.Location = new Point(88, 93);
-            pictureBox2.Margin = new Padding(3, 2, 3, 2);
+            pictureBox2.Location = new Point(101, 124);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(1224, 529);
+            pictureBox2.Size = new Size(1399, 705);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox2.TabIndex = 46;
             pictureBox2.TabStop = false;
@@ -268,9 +265,9 @@
             label8.BackColor = Color.Linen;
             label8.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label8.ForeColor = Color.SaddleBrown;
-            label8.Location = new Point(511, 27);
+            label8.Location = new Point(584, 36);
             label8.Name = "label8";
-            label8.Size = new Size(378, 41);
+            label8.Size = new Size(467, 50);
             label8.TabIndex = 56;
             label8.Text = "O P R E T   N Y   K U N D E";
             // 
@@ -278,10 +275,9 @@
             // 
             pictureBox7.BackColor = Color.Linen;
             pictureBox7.Image = Properties.Resources.PageBox;
-            pictureBox7.Location = new Point(364, 487);
-            pictureBox7.Margin = new Padding(3, 2, 3, 2);
+            pictureBox7.Location = new Point(416, 649);
             pictureBox7.Name = "pictureBox7";
-            pictureBox7.Size = new Size(673, 66);
+            pictureBox7.Size = new Size(769, 88);
             pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox7.TabIndex = 57;
             pictureBox7.TabStop = false;
@@ -292,59 +288,68 @@
             label9.BackColor = Color.AntiqueWhite;
             label9.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label9.ForeColor = Color.SaddleBrown;
-            label9.Location = new Point(651, 498);
+            label9.Location = new Point(744, 664);
             label9.Name = "label9";
-            label9.Size = new Size(99, 15);
+            label9.Size = new Size(128, 20);
             label9.TabIndex = 58;
             label9.Text = "K U N D E T Y P E";
             // 
             // textBoxLastName
             // 
             textBoxLastName.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBoxLastName.Location = new Point(580, 220);
-            textBoxLastName.MaxLength = 8;
+            textBoxLastName.Location = new Point(663, 293);
+            textBoxLastName.Margin = new Padding(3, 4, 3, 4);
+            textBoxLastName.MaxLength = 0;
             textBoxLastName.Name = "textBoxLastName";
-            textBoxLastName.Size = new Size(240, 23);
+            textBoxLastName.Size = new Size(274, 27);
             textBoxLastName.TabIndex = 59;
+            textBoxLastName.KeyPress += textBoxLastName_KeyPress;
             // 
             // textBoxEmail
             // 
             textBoxEmail.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBoxEmail.Location = new Point(580, 295);
-            textBoxEmail.MaxLength = 8;
+            textBoxEmail.Location = new Point(663, 393);
+            textBoxEmail.Margin = new Padding(3, 4, 3, 4);
+            textBoxEmail.MaxLength = 0;
             textBoxEmail.Name = "textBoxEmail";
-            textBoxEmail.Size = new Size(240, 23);
+            textBoxEmail.Size = new Size(274, 27);
             textBoxEmail.TabIndex = 60;
+            textBoxEmail.KeyPress += textBoxEmail_KeyPress;
             // 
             // textBoxAddress
             // 
             textBoxAddress.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBoxAddress.Location = new Point(580, 370);
-            textBoxAddress.MaxLength = 8;
+            textBoxAddress.Location = new Point(663, 493);
+            textBoxAddress.Margin = new Padding(3, 4, 3, 4);
+            textBoxAddress.MaxLength = 0;
             textBoxAddress.Name = "textBoxAddress";
-            textBoxAddress.Size = new Size(240, 23);
+            textBoxAddress.Size = new Size(274, 27);
             textBoxAddress.TabIndex = 61;
+            textBoxAddress.KeyPress += textBoxAddress_KeyPress;
             // 
             // textBoxPhoneNumber
             // 
             textBoxPhoneNumber.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBoxPhoneNumber.Location = new Point(580, 445);
-            textBoxPhoneNumber.MaxLength = 8;
+            textBoxPhoneNumber.Location = new Point(663, 593);
+            textBoxPhoneNumber.Margin = new Padding(3, 4, 3, 4);
+            textBoxPhoneNumber.MaxLength = 0;
             textBoxPhoneNumber.Name = "textBoxPhoneNumber";
-            textBoxPhoneNumber.Size = new Size(240, 23);
+            textBoxPhoneNumber.Size = new Size(274, 27);
             textBoxPhoneNumber.TabIndex = 62;
+            textBoxPhoneNumber.KeyPress += textBoxPhoneNumber_KeyPress;
             // 
             // comboBoxType
             // 
             comboBoxType.FormattingEnabled = true;
-            comboBoxType.Location = new Point(580, 516);
+            comboBoxType.Location = new Point(663, 688);
+            comboBoxType.Margin = new Padding(3, 4, 3, 4);
             comboBoxType.Name = "comboBoxType";
-            comboBoxType.Size = new Size(240, 23);
+            comboBoxType.Size = new Size(274, 28);
             comboBoxType.TabIndex = 64;
             // 
             // CreateCustomerUpdate
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(comboBoxType);
             Controls.Add(textBoxPhoneNumber);
@@ -373,7 +378,7 @@
             Controls.Add(label6);
             Margin = new Padding(0);
             Name = "CreateCustomerUpdate";
-            Size = new Size(1400, 634);
+            Size = new Size(1600, 845);
             ((System.ComponentModel.ISupportInitialize)submitButton).EndInit();
             ((System.ComponentModel.ISupportInitialize)cancelButton).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
