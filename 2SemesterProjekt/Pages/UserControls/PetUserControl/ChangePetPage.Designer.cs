d73b64f36bcae15b661021f881b7d257c8f6b68f
AskGamKru <kruseaske@gmail.com>
2025-05-20 10:56:24 +0200

    Change, Update and Find Pet

--- 2SemesterProjekt/Pages/UserControls/PetUserControl/ChangePetPage.Designer.cs
+++ 2SemesterProjekt/Pages/UserControls/PetUserControl/ChangePetPage.Designer.cs
@@ -29,8 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChangePetPage));
-            label2 = new Label();
-            pictureBox1 = new PictureBox();
             changeButton = new PictureBox();
             flowPanel = new FlowLayoutPanel();
             textBoxCustomerPhoneNumberOrName = new TextBox();
@@ -38,35 +36,18 @@
             findAllButton = new PictureBox();
             label1 = new Label();
             pictureBox3 = new PictureBox();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
             pictureBox4 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            pictureBox2 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)changeButton).BeginInit();
             ((System.ComponentModel.ISupportInitialize)customerSearchButton).BeginInit();
             ((System.ComponentModel.ISupportInitialize)findAllButton).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.BackColor = Color.Linen;
-            label2.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.ForeColor = Color.SaddleBrown;
-            label2.Location = new Point(830, 108);
-            label2.Name = "label2";
-            label2.Size = new Size(841, 81);
-            label2.TabIndex = 26;
-            label2.Text = "O P D A T E R   K Æ L E D Y R";
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(84, 77);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(2332, 144);
-            pictureBox1.TabIndex = 27;
-            pictureBox1.TabStop = false;
             // 
             // changeButton
             // 
@@ -83,103 +64,139 @@
             flowPanel.AutoScroll = true;
             flowPanel.BackColor = Color.Linen;
             flowPanel.BorderStyle = BorderStyle.FixedSingle;
-            flowPanel.Location = new Point(609, 452);
+            flowPanel.Location = new Point(416, 240);
             flowPanel.Name = "flowPanel";
-            flowPanel.Size = new Size(1282, 867);
-            flowPanel.TabIndex = 43;
+            flowPanel.Size = new Size(769, 562);
+            flowPanel.TabIndex = 63;
             // 
             // textBoxCustomerPhoneNumberOrName
             // 
-            textBoxCustomerPhoneNumberOrName.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBoxCustomerPhoneNumberOrName.Location = new Point(845, 355);
+            textBoxCustomerPhoneNumberOrName.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBoxCustomerPhoneNumberOrName.Location = new Point(570, 191);
             textBoxCustomerPhoneNumberOrName.Margin = new Padding(3, 4, 3, 4);
             textBoxCustomerPhoneNumberOrName.MaxLength = 8;
             textBoxCustomerPhoneNumberOrName.Name = "textBoxCustomerPhoneNumberOrName";
-            textBoxCustomerPhoneNumberOrName.Size = new Size(457, 42);
-            textBoxCustomerPhoneNumberOrName.TabIndex = 42;
+            textBoxCustomerPhoneNumberOrName.Size = new Size(274, 27);
+            textBoxCustomerPhoneNumberOrName.TabIndex = 62;
             // 
             // customerSearchButton
             // 
             customerSearchButton.BackColor = Color.AntiqueWhite;
-            customerSearchButton.Image = (Image)resources.GetObject("customerSearchButton.Image");
-            customerSearchButton.Location = new Point(1323, 359);
+            customerSearchButton.Image = Properties.Resources.SearchButton;
+            customerSearchButton.Location = new Point(850, 194);
             customerSearchButton.Margin = new Padding(3, 4, 3, 4);
             customerSearchButton.Name = "customerSearchButton";
-            customerSearchButton.Size = new Size(79, 35);
-            customerSearchButton.TabIndex = 41;
+            customerSearchButton.Size = new Size(47, 21);
+            customerSearchButton.SizeMode = PictureBoxSizeMode.StretchImage;
+            customerSearchButton.TabIndex = 61;
             customerSearchButton.TabStop = false;
             // 
             // findAllButton
             // 
-            findAllButton.Image = (Image)resources.GetObject("findAllButton.Image");
-            findAllButton.Location = new Point(1490, 359);
+            findAllButton.Image = Properties.Resources.FindAllButton;
+            findAllButton.Location = new Point(931, 194);
             findAllButton.Name = "findAllButton";
-            findAllButton.Size = new Size(165, 35);
-            findAllButton.TabIndex = 40;
+            findAllButton.Size = new Size(99, 21);
+            findAllButton.SizeMode = PictureBoxSizeMode.StretchImage;
+            findAllButton.TabIndex = 60;
             findAllButton.TabStop = false;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.BackColor = Color.AntiqueWhite;
-            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.SaddleBrown;
-            label1.Location = new Point(771, 307);
+            label1.Location = new Point(483, 166);
             label1.Name = "label1";
-            label1.Size = new Size(958, 31);
-            label1.TabIndex = 39;
+            label1.Size = new Size(635, 20);
+            label1.TabIndex = 59;
             label1.Text = "T R Y K   F I N D   A L L E   E L L E R   S Ø G   P Å   K U N D E N S   T E L E F O N N U M M E R";
             // 
             // pictureBox3
             // 
             pictureBox3.BackColor = Color.Linen;
-            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
-            pictureBox3.Location = new Point(609, 273);
+            pictureBox3.Image = Properties.Resources.PageBox;
+            pictureBox3.Location = new Point(416, 146);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(1282, 148);
-            pictureBox3.TabIndex = 38;
+            pictureBox3.Size = new Size(769, 88);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 58;
             pictureBox3.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Linen;
+            label2.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.SaddleBrown;
+            label2.Location = new Point(544, 34);
+            label2.Name = "label2";
+            label2.Size = new Size(513, 50);
+            label2.TabIndex = 56;
+            label2.Text = "O P D A T E R   K Æ L E D Y R";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.PageTitelBackground;
+            pictureBox1.Location = new Point(101, 17);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1399, 86);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 57;
+            pictureBox1.TabStop = false;
             // 
             // pictureBox4
             // 
-            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
-            pictureBox4.Location = new Point(84, 242);
+            pictureBox4.Image = Properties.Resources.PageBackground;
+            pictureBox4.Location = new Point(101, 122);
             pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(2332, 1116);
-            pictureBox4.TabIndex = 44;
+            pictureBox4.Size = new Size(1399, 705);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 64;
             pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.ChangeButtonGreyedOut;
+            pictureBox2.Location = new Point(1221, 750);
+            pictureBox2.Margin = new Padding(3, 4, 3, 4);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(240, 55);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 65;
+            pictureBox2.TabStop = false;
             // 
             // ChangePetPage
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(changeButton);
+            Controls.Add(pictureBox2);
             Controls.Add(flowPanel);
             Controls.Add(textBoxCustomerPhoneNumberOrName);
             Controls.Add(customerSearchButton);
             Controls.Add(findAllButton);
             Controls.Add(label1);
             Controls.Add(pictureBox3);
-            Controls.Add(pictureBox4);
             Controls.Add(label2);
             Controls.Add(pictureBox1);
+            Controls.Add(pictureBox4);
+            Controls.Add(changeButton);
             Margin = new Padding(0);
             Name = "ChangePetPage";
-            Size = new Size(2500, 1580);
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            Size = new Size(1600, 845);
             ((System.ComponentModel.ISupportInitialize)changeButton).EndInit();
             ((System.ComponentModel.ISupportInitialize)customerSearchButton).EndInit();
             ((System.ComponentModel.ISupportInitialize)findAllButton).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private Label label2;
-        private PictureBox pictureBox1;
         private PictureBox changeButton;
         private FlowLayoutPanel flowPanel;
         private TextBox textBoxCustomerPhoneNumberOrName;
@@ -187,6 +204,9 @@
         private PictureBox findAllButton;
         private Label label1;
         private PictureBox pictureBox3;
+        private Label label2;
+        private PictureBox pictureBox1;
         private PictureBox pictureBox4;
+        private PictureBox pictureBox2;
     }
 }
--- 2SemesterProjekt/Pages/UserControls/PetUserControl/FindPetPage.Designer.cs
+++ 2SemesterProjekt/Pages/UserControls/PetUserControl/FindPetPage.Designer.cs
@@ -28,83 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label2 = new Label();
-            pictureBox1 = new PictureBox();
             flowPanel = new FlowLayoutPanel();
             textBoxCustomerPhoneNumberOrName = new TextBox();
             customerSearchButton = new PictureBox();
             findAllButton = new PictureBox();
             label1 = new Label();
             pictureBox3 = new PictureBox();
-            pictureBox2 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox4 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)customerSearchButton).BeginInit();
             ((System.ComponentModel.ISupportInitialize)findAllButton).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             SuspendLayout();
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.BackColor = Color.Linen;
-            label2.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.ForeColor = Color.SaddleBrown;
-            label2.Location = new Point(922, 108);
-            label2.Name = "label2";
-            label2.Size = new Size(657, 81);
-            label2.TabIndex = 26;
-            label2.Text = "F I N D   K Æ L E D Y R";
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Image = Properties.Resources.PageTitelBackground;
-            pictureBox1.Location = new Point(84, 77);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(2332, 144);
-            pictureBox1.TabIndex = 27;
-            pictureBox1.TabStop = false;
             // 
             // flowPanel
             // 
             flowPanel.AutoScroll = true;
             flowPanel.BackColor = Color.Linen;
             flowPanel.BorderStyle = BorderStyle.FixedSingle;
-            flowPanel.Location = new Point(609, 452);
+            flowPanel.Location = new Point(416, 240);
             flowPanel.Name = "flowPanel";
-            flowPanel.Size = new Size(1282, 1027);
-            flowPanel.TabIndex = 44;
+            flowPanel.Size = new Size(769, 562);
+            flowPanel.TabIndex = 54;
             // 
             // textBoxCustomerPhoneNumberOrName
             // 
-            textBoxCustomerPhoneNumberOrName.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBoxCustomerPhoneNumberOrName.ForeColor = SystemColors.InactiveCaption;
-            textBoxCustomerPhoneNumberOrName.Location = new Point(845, 355);
+            textBoxCustomerPhoneNumberOrName.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBoxCustomerPhoneNumberOrName.Location = new Point(570, 191);
             textBoxCustomerPhoneNumberOrName.Margin = new Padding(3, 4, 3, 4);
             textBoxCustomerPhoneNumberOrName.MaxLength = 8;
             textBoxCustomerPhoneNumberOrName.Name = "textBoxCustomerPhoneNumberOrName";
-            textBoxCustomerPhoneNumberOrName.Size = new Size(457, 42);
-            textBoxCustomerPhoneNumberOrName.TabIndex = 43;
-            textBoxCustomerPhoneNumberOrName.Text = "Søg på navn eller ID";
+            textBoxCustomerPhoneNumberOrName.Size = new Size(274, 27);
+            textBoxCustomerPhoneNumberOrName.TabIndex = 53;
             // 
             // customerSearchButton
             // 
             customerSearchButton.BackColor = Color.AntiqueWhite;
             customerSearchButton.Image = Properties.Resources.SearchButton;
-            customerSearchButton.Location = new Point(1323, 359);
+            customerSearchButton.Location = new Point(850, 194);
             customerSearchButton.Margin = new Padding(3, 4, 3, 4);
             customerSearchButton.Name = "customerSearchButton";
-            customerSearchButton.Size = new Size(79, 35);
-            customerSearchButton.TabIndex = 42;
+            customerSearchButton.Size = new Size(47, 21);
+            customerSearchButton.SizeMode = PictureBoxSizeMode.StretchImage;
+            customerSearchButton.TabIndex = 52;
             customerSearchButton.TabStop = false;
+            customerSearchButton.Click += SearchForPetByPhoneNumber;
             // 
             // findAllButton
             // 
             findAllButton.Image = Properties.Resources.FindAllButton;
-            findAllButton.Location = new Point(1490, 359);
+            findAllButton.Location = new Point(931, 194);
             findAllButton.Name = "findAllButton";
-            findAllButton.Size = new Size(165, 35);
-            findAllButton.TabIndex = 41;
+            findAllButton.Size = new Size(99, 21);
+            findAllButton.SizeMode = PictureBoxSizeMode.StretchImage;
+            findAllButton.TabIndex = 51;
             findAllButton.TabStop = false;
             findAllButton.Click += findAllButton_Click;
             // 
@@ -112,33 +92,56 @@
             // 
             label1.AutoSize = true;
             label1.BackColor = Color.AntiqueWhite;
-            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.SaddleBrown;
-            label1.Location = new Point(771, 307);
+            label1.Location = new Point(483, 166);
             label1.Name = "label1";
-            label1.Size = new Size(958, 31);
-            label1.TabIndex = 40;
+            label1.Size = new Size(635, 20);
+            label1.TabIndex = 50;
             label1.Text = "T R Y K   F I N D   A L L E   E L L E R   S Ø G   P Å   K U N D E N S   T E L E F O N N U M M E R";
             // 
             // pictureBox3
             // 
             pictureBox3.BackColor = Color.Linen;
             pictureBox3.Image = Properties.Resources.PageBox;
-            pictureBox3.Location = new Point(609, 277);
+            pictureBox3.Location = new Point(416, 146);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(1282, 148);
-            pictureBox3.TabIndex = 39;
+            pictureBox3.Size = new Size(769, 88);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 49;
             pictureBox3.TabStop = false;
             // 
-            // pictureBox2
+            // label2
             // 
-            pictureBox2.Image = Properties.Resources.PageBackground;
-            pictureBox2.Location = new Point(84, 242);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(2332, 1263);
-            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox2.TabIndex = 38;
-            pictureBox2.TabStop = false;
+            label2.AutoSize = true;
+            label2.BackColor = Color.Linen;
+            label2.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.SaddleBrown;
+            label2.Location = new Point(544, 34);
+            label2.Name = "label2";
+            label2.Size = new Size(513, 50);
+            label2.TabIndex = 47;
+            label2.Text = "O P D A T E R   K Æ L E D Y R";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.PageTitelBackground;
+            pictureBox1.Location = new Point(101, 17);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1399, 86);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 48;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.PageBackground;
+            pictureBox4.Location = new Point(101, 122);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(1399, 705);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 55;
+            pictureBox4.TabStop = false;
             // 
             // FindPetPage
             // 
@@ -150,32 +153,32 @@
             Controls.Add(findAllButton);
             Controls.Add(label1);
             Controls.Add(pictureBox3);
-            Controls.Add(pictureBox2);
             Controls.Add(label2);
             Controls.Add(pictureBox1);
+            Controls.Add(pictureBox4);
             Margin = new Padding(0);
             Name = "FindPetPage";
-            Size = new Size(2500, 1580);
+            Size = new Size(1600, 845);
             Load += FindPetPage_Load;
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)customerSearchButton).EndInit();
             ((System.ComponentModel.ISupportInitialize)findAllButton).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Label label2;
-        private PictureBox pictureBox1;
         private FlowLayoutPanel flowPanel;
         private TextBox textBoxCustomerPhoneNumberOrName;
         private PictureBox customerSearchButton;
         private PictureBox findAllButton;
         private Label label1;
         private PictureBox pictureBox3;
-        private PictureBox pictureBox2;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox4;
     }
 }
