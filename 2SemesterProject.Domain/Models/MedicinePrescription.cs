bb087d7de9f3eef6f11c9a5e9aaadf8e09be41f3
MikkelRLarsen <Thenuker95@gmail.com>
2025-05-22 11:02:33 +0200

    Began model building

---
+++ 2SemesterProject.Domain/Models/MedicinePrescription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2SemesterProjekt.Domain.Models
+{
+	public class MedicinePrescription
+	{
+		public int MedicinePrescriptionID { get; init; }
+		public int ExaminationID { get; private set; }
+		public int MedicineDetailsID { get; private set; }
+	}
+}
