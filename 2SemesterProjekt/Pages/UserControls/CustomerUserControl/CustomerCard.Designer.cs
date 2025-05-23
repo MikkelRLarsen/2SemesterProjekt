7368f48222b8ea81ebf3bc41b0067d550119fcd0
AskGamKru <kruseaske@gmail.com>
2025-05-23 10:52:24 +0200

    Redesigned CustomerCard to match examinationCard

--- 2SemesterProjekt/Pages/UserControls/CustomerUserControl/CustomerCard.Designer.cs
+++ 2SemesterProjekt/Pages/UserControls/CustomerUserControl/CustomerCard.Designer.cs
@@ -40,17 +40,20 @@
             label5 = new Label();
             customerEmailLabel = new Label();
             label7 = new Label();
+            pictureBox = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)profilePicture).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
             SuspendLayout();
             // 
             // profilePicture
             // 
             profilePicture.Anchor = AnchorStyles.Left;
+            profilePicture.BackColor = Color.AntiqueWhite;
             profilePicture.Image = Properties.Resources.member;
-            profilePicture.Location = new Point(6, 4);
+            profilePicture.Location = new Point(15, 15);
             profilePicture.Margin = new Padding(3, 4, 3, 4);
             profilePicture.Name = "profilePicture";
-            profilePicture.Size = new Size(68, 80);
+            profilePicture.Size = new Size(81, 81);
             profilePicture.SizeMode = PictureBoxSizeMode.StretchImage;
             profilePicture.TabIndex = 0;
             profilePicture.TabStop = false;
@@ -58,20 +61,24 @@
             // customerIdLabel
             // 
             customerIdLabel.AutoSize = true;
-            customerIdLabel.Font = new Font("Gadugi", 7F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            customerIdLabel.Location = new Point(144, 7);
+            customerIdLabel.BackColor = Color.AntiqueWhite;
+            customerIdLabel.Font = new Font("Gadugi", 8F, FontStyle.Bold);
+            customerIdLabel.ForeColor = Color.SaddleBrown;
+            customerIdLabel.Location = new Point(192, 15);
             customerIdLabel.Name = "customerIdLabel";
-            customerIdLabel.Size = new Size(21, 16);
+            customerIdLabel.Size = new Size(24, 18);
             customerIdLabel.TabIndex = 2;
             customerIdLabel.Text = "##";
             // 
             // customerAddressLabel
             // 
             customerAddressLabel.AutoSize = true;
-            customerAddressLabel.Font = new Font("Gadugi", 7F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            customerAddressLabel.Location = new Point(144, 47);
+            customerAddressLabel.BackColor = Color.AntiqueWhite;
+            customerAddressLabel.Font = new Font("Gadugi", 8F, FontStyle.Bold);
+            customerAddressLabel.ForeColor = Color.SaddleBrown;
+            customerAddressLabel.Location = new Point(192, 79);
             customerAddressLabel.Name = "customerAddressLabel";
-            customerAddressLabel.Size = new Size(105, 16);
+            customerAddressLabel.Size = new Size(126, 18);
             customerAddressLabel.TabIndex = 6;
             customerAddressLabel.Text = "CustomerAddress";
             customerAddressLabel.UseMnemonic = false;
@@ -79,60 +86,72 @@
             // label
             // 
             label.AutoSize = true;
-            label.Font = new Font("Gadugi", 7F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label.Location = new Point(80, 7);
+            label.BackColor = Color.AntiqueWhite;
+            label.Font = new Font("Gadugi", 8F, FontStyle.Bold);
+            label.ForeColor = Color.SaddleBrown;
+            label.Location = new Point(118, 15);
             label.Name = "label";
-            label.Size = new Size(58, 16);
+            label.Size = new Size(68, 18);
             label.TabIndex = 1;
             label.Text = "Kundenr:";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Gadugi", 7F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(80, 47);
+            label1.BackColor = Color.AntiqueWhite;
+            label1.Font = new Font("Gadugi", 8F, FontStyle.Bold);
+            label1.ForeColor = Color.SaddleBrown;
+            label1.Location = new Point(118, 79);
             label1.Name = "label1";
-            label1.Size = new Size(54, 16);
+            label1.Size = new Size(65, 18);
             label1.TabIndex = 5;
             label1.Text = "Adresse:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Gadugi", 7F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(270, 7);
+            label2.BackColor = Color.AntiqueWhite;
+            label2.Font = new Font("Gadugi", 8F, FontStyle.Bold);
+            label2.ForeColor = Color.SaddleBrown;
+            label2.Location = new Point(15, 111);
             label2.Name = "label2";
-            label2.Size = new Size(116, 16);
+            label2.Size = new Size(139, 18);
             label2.TabIndex = 7;
             label2.Text = "Kontaktoplysninger";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Gadugi", 7F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(270, 27);
+            label3.BackColor = Color.AntiqueWhite;
+            label3.Font = new Font("Gadugi", 8F, FontStyle.Bold);
+            label3.ForeColor = Color.SaddleBrown;
+            label3.Location = new Point(15, 140);
             label3.Name = "label3";
-            label3.Size = new Size(25, 16);
+            label3.Size = new Size(62, 18);
             label3.TabIndex = 8;
-            label3.Text = "Tlf:";
+            label3.Text = "Telefon:";
             // 
             // customerPhoneNumberLabel
             // 
             customerPhoneNumberLabel.AutoSize = true;
-            customerPhoneNumberLabel.Font = new Font("Gadugi", 7F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            customerPhoneNumberLabel.Location = new Point(323, 27);
+            customerPhoneNumberLabel.BackColor = Color.AntiqueWhite;
+            customerPhoneNumberLabel.Font = new Font("Gadugi", 8F, FontStyle.Bold);
+            customerPhoneNumberLabel.ForeColor = Color.SaddleBrown;
+            customerPhoneNumberLabel.Location = new Point(118, 140);
             customerPhoneNumberLabel.Name = "customerPhoneNumberLabel";
-            customerPhoneNumberLabel.Size = new Size(63, 16);
+            customerPhoneNumberLabel.Size = new Size(72, 18);
             customerPhoneNumberLabel.TabIndex = 9;
             customerPhoneNumberLabel.Text = "12345678";
             // 
             // customerNameLabel
             // 
             customerNameLabel.AutoSize = true;
-            customerNameLabel.Font = new Font("Gadugi", 7F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            customerNameLabel.Location = new Point(144, 27);
+            customerNameLabel.BackColor = Color.AntiqueWhite;
+            customerNameLabel.Font = new Font("Gadugi", 8F, FontStyle.Bold);
+            customerNameLabel.ForeColor = Color.SaddleBrown;
+            customerNameLabel.Location = new Point(192, 49);
             customerNameLabel.Name = "customerNameLabel";
-            customerNameLabel.Size = new Size(94, 16);
+            customerNameLabel.Size = new Size(112, 18);
             customerNameLabel.TabIndex = 4;
             customerNameLabel.Text = "CustomerName";
             customerNameLabel.UseMnemonic = false;
@@ -140,20 +159,24 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new Font("Gadugi", 7F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.Location = new Point(80, 27);
+            label5.BackColor = Color.AntiqueWhite;
+            label5.Font = new Font("Gadugi", 8F, FontStyle.Bold);
+            label5.ForeColor = Color.SaddleBrown;
+            label5.Location = new Point(118, 49);
             label5.Name = "label5";
-            label5.Size = new Size(39, 16);
+            label5.Size = new Size(47, 18);
             label5.TabIndex = 3;
             label5.Text = "Navn:";
             // 
             // customerEmailLabel
             // 
             customerEmailLabel.AutoSize = true;
-            customerEmailLabel.Font = new Font("Gadugi", 7F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            customerEmailLabel.Location = new Point(323, 47);
+            customerEmailLabel.BackColor = Color.AntiqueWhite;
+            customerEmailLabel.Font = new Font("Gadugi", 8F, FontStyle.Bold);
+            customerEmailLabel.ForeColor = Color.SaddleBrown;
+            customerEmailLabel.Location = new Point(118, 163);
             customerEmailLabel.Name = "customerEmailLabel";
-            customerEmailLabel.Size = new Size(101, 16);
+            customerEmailLabel.Size = new Size(123, 18);
             customerEmailLabel.TabIndex = 11;
             customerEmailLabel.Text = "mail@outlook.dk";
             customerEmailLabel.UseMnemonic = false;
@@ -161,12 +184,24 @@
             // label7
             // 
             label7.AutoSize = true;
-            label7.Font = new Font("Gadugi", 7F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label7.Location = new Point(270, 47);
+            label7.BackColor = Color.AntiqueWhite;
+            label7.Font = new Font("Gadugi", 8F, FontStyle.Bold);
+            label7.ForeColor = Color.SaddleBrown;
+            label7.Location = new Point(15, 163);
             label7.Name = "label7";
-            label7.Size = new Size(39, 16);
+            label7.Size = new Size(48, 18);
             label7.TabIndex = 10;
             label7.Text = "Email:";
+            // 
+            // pictureBox
+            // 
+            pictureBox.Image = Properties.Resources.CardExamination;
+            pictureBox.Location = new Point(3, 3);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(655, 192);
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox.TabIndex = 40;
+            pictureBox.TabStop = false;
             // 
             // CustomerCard
             // 
@@ -185,10 +220,12 @@
             Controls.Add(label);
             Controls.Add(customerAddressLabel);
             Controls.Add(customerIdLabel);
+            Controls.Add(pictureBox);
             Margin = new Padding(3, 4, 3, 4);
             Name = "CustomerCard";
-            Size = new Size(480, 88);
+            Size = new Size(655, 192);
             ((System.ComponentModel.ISupportInitialize)profilePicture).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -207,7 +244,6 @@
 		private Label label5;
 		private Label customerEmailLabel;
 		private Label label7;
-
-
-	}
+        private PictureBox pictureBox;
+    }
 }
